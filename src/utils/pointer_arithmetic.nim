117440f443eee4c6c7c72641006841456cc381c2
Mamy Ratsimbazafy <mamy.ratsimbazafy_dev@gadz.org>
2017-04-16 22:18:09 +0200

    Commenting to limit XDeclaredButNotUsed https://github.com/mratsim/Arraymancer/issues/2

--- src/utils/pointer_arithmetic.nim
+++ src/utils/pointer_arithmetic.nim
@@ -3,26 +3,30 @@
 
 
 template ptrMath(body: untyped) =
+  {.push hint[XDeclaredButNotUsed]: off.}
+  # XDeclaredButNotUsed pending: https://github.com/nim-lang/Nim/issues/4044
+
   template `+`[T](p: ptr T, off: int): ptr T =
     cast[ptr type(p[])](cast[ByteAddress](p) +% off * sizeof(p[]))
   
   template `+=`[T](p: ptr T, off: int) =
     p = p + off
   
-  template `-`[T](p: ptr T, off: int): ptr T =
-    cast[ptr type(p[])](cast[ByteAddress](p) -% off * sizeof(p[]))
-  
-  template `-=`[T](p: ptr T, off: int) =
-    p = p - off
-  
-  template `[]`[T](p: ptr T, off: int): T =
-    (p + off)[]
-  
-  template `[]=`[T](p: ptr T, off: int, val: T) =
-    (p + off)[] = val
+  # template `-`[T](p: ptr T, off: int): ptr T =
+  #   cast[ptr type(p[])](cast[ByteAddress](p) -% off * sizeof(p[]))
+  # 
+  # template `-=`[T](p: ptr T, off: int) =
+  #   p = p - off
+  # 
+  # template `[]`[T](p: ptr T, off: int): T =
+  #   (p + off)[]
+  # 
+  # template `[]=`[T](p: ptr T, off: int, val: T) =
+  #   (p + off)[] = val
 
   ## TODO: Thorougly test this, especially with negative offset
   template `-`[T](off_p: ptr T, p: ptr T): int =
     (cast[ByteAddress](off_p) -% cast[ByteAddress](p)) div sizeof(p[])
 
+  {.pop.}
   body

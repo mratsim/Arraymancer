72198e201a6a883ea973b1283c9f233d85ace5d4
Mamy Ratsimbazafy <mamy.ratsimbazafy_dev@gadz.org>
2017-09-09 20:31:17 +0200

    Use generics and inlining for CUBLAS API

--- src/arraymancer/backend/cublas.nim
+++ src/arraymancer/backend/cublas.nim
@@ -16,89 +16,117 @@
 # Redefinition of imported CUDA proc with standard name
 # and use of streams for parallel async processing
 
+# L1 BLAS
+
 # Vector copy
-proc cublas_copy(n: int; x: ptr float32; incx: int;
-                         y: ptr float32; incy: int): cublasStatus_t =
+proc cublas_copy[T: SomeReal](
+  n: int; x: ptr T; incx: int;
+  y: ptr T; incy: int): cublasStatus_t {.inline.}=
 
   check cublasSetStream(defaultHandle, defaultStream)
-  cublasScopy(defaultHandle, n.cint, x, incx.cint, y, incy.cint)
 
-proc cublas_copy(n: int; x: ptr float64; incx: int;
-                         y: ptr float64; incy: int): cublasStatus_t =
-
-  check cublasSetStream(defaultHandle, defaultStream)
-  cublasDcopy(defaultHandle, n.cint, x, incx.cint, y, incy.cint)
+  when T is float32:
+    cublasScopy(defaultHandle, n.cint, x, incx.cint, y, incy.cint)
+  elif T is float64:
+    cublasDcopy(defaultHandle, n.cint, x, incx.cint, y, incy.cint)
+  else:
+    raise newException(ValueError, "Unreachable")
 
 # Vector dot product
-proc cublas_dot(n: int;
-                x: ptr float32; incx: int;
-                y: ptr float32; incy: int;
-                output: ptr float32): cublasStatus_t =
+proc cublas_dot[T: SomeReal](
+  n: int;
+  x: ptr T; incx: int;
+  y: ptr T; incy: int;
+  output: ptr T): cublasStatus_t {.inline.}=
 
   check cublasSetStream(defaultHandle, defaultStream)
-  cublasSdot(defaultHandle, n.cint, x, incx.cint, y, incy.cint, output)
 
-proc cublas_dot(n: int;
-                x: ptr float64; incx: int;
-                y: ptr float64; incy: int;
-                output: ptr float64): cublasStatus_t =
-
-  check cublasSetStream(defaultHandle, defaultStream)
-  cublasDdot(defaultHandle, n.cint, x, incx.cint, y, incy.cint, output)
+  when T is float32:
+    cublasSdot(defaultHandle, n.cint, x, incx.cint, y, incy.cint, output)
+  elif T is float64:
+    cublasDdot(defaultHandle, n.cint, x, incx.cint, y, incy.cint, output)
+  else:
+    raise newException(ValueError, "Unreachable")
 
 # Vector addition
-proc cublas_axpy( n: int;
-                  alpha: ptr float32;
-                  x: ptr float32; incx: int;
-                  y: ptr float32; incy: int): cublasStatus_t =
+proc cublas_axpy[T: SomeReal](
+  n: int;
+  alpha: ptr T;
+  x: ptr T; incx: int;
+  y: ptr T; incy: int): cublasStatus_t {.inline.}=
 
   check cublasSetStream(defaultHandle, defaultStream)
-  cublasSaxpy(defaultHandle, n.cint, alpha, x, incx.cint, y, incy.cint)
 
-proc cublas_axpy( n: int;
-                  alpha: ptr float64;
-                  x: ptr float64; incx: int;
-                  y: ptr float64; incy: int): cublasStatus_t =
-
-  check cublasSetStream(defaultHandle, defaultStream)
-  cublasDaxpy(defaultHandle, n.cint, alpha, x, incx.cint, y, incy.cint)
-
-# Matrix addition (non-standard BLAS)
-proc cublas_geam( transa, transb: cublasOperation_t;
-                  m, n: int;
-                  alpha: ptr float32; A: ptr float32; lda: int;
-                  beta: ptr float32; B: ptr float32; ldb: int;
-                  C: ptr float32; ldc: int): cublasStatus_t =
-
-  check cublasSetStream(defaultHandle, defaultStream)
-  cublasSgeam(defaultHandle,
-              transa, transb, m.cint, n.cint,
-              alpha, A, lda.cint,
-              beta, B, ldb.cint,
-              C, ldc.cint)
-
-proc cublas_geam( transa, transb: cublasOperation_t;
-                  m, n: int;
-                  alpha: ptr float64; A: ptr float64; lda: int;
-                  beta: ptr float64; B: ptr float64; ldb: int;
-                  C: ptr float64; ldc: int): cublasStatus_t =
-
-  check cublasSetStream(defaultHandle, defaultStream)
-  cublasDgeam(defaultHandle,
-              transa, transb, m.cint, n.cint,
-              alpha, A, lda.cint,
-              beta, B, ldb.cint,
-              C, ldc.cint)
+  when T is float32:
+    cublasSaxpy(defaultHandle, n.cint, alpha, x, incx.cint, y, incy.cint)
+  elif T is float64:
+    cublasDaxpy(defaultHandle, n.cint, alpha, x, incx.cint, y, incy.cint)
+  else:
+    raise newException(ValueError, "Unreachable")
 
 # Scalar multiplication
-proc cublas_scal( n: int; alpha: ptr float32;
-                  x: ptr float32; incx: int): cublasStatus_t =
+proc cublas_scal[T: SomeReal](
+  n: int; alpha: ptr T;
+  x: ptr T; incx: int): cublasStatus_t {.inline.}=
 
   check cublasSetStream(defaultHandle, defaultStream)
-  cublasSscal(defaultHandle, n.cint, alpha, x, incx.cint)
 
-proc cublas_scal( n: int; alpha: ptr float64;
-                  x: ptr float64; incx: int): cublasStatus_t =
+  when T is float32:
+    cublasSscal(defaultHandle, n.cint, alpha, x, incx.cint)
+  elif T is float64:
+    cublasDscal(defaultHandle, n.cint, alpha, x, incx.cint)
+  else:
+    raise newException(ValueError, "Unreachable")
+
+# BLAS extension (L1-like)
+# Matrix addition (non-standard BLAS)
+proc cublas_geam[T: SomeReal](
+  transa, transb: cublasOperation_t;
+  m, n: int;
+  alpha: ptr T; A: ptr T; lda: int;
+  beta: ptr T; B: ptr T; ldb: int;
+  C: ptr T; ldc: int): cublasStatus_t {.inline.}=
 
   check cublasSetStream(defaultHandle, defaultStream)
-  cublasDscal(defaultHandle, n.cint, alpha, x, incx.cint)
+
+  when T is float32:
+    cublasSgeam(defaultHandle,
+            transa, transb, m.cint, n.cint,
+            alpha, A, lda.cint,
+            beta, B, ldb.cint,
+            C, ldc.cint)
+  elif T is float64:
+    cublasDgeam(defaultHandle,
+                transa, transb, m.cint, n.cint,
+                alpha, A, lda.cint,
+                beta, B, ldb.cint,
+                C, ldc.cint)
+  else:
+    raise newException(ValueError, "Unreachable")
+
+# L2 BLAS
+
+# L3 BLAS
+proc cublas_gemm*[T: SomeReal](
+  transa, transb: cublasOperation_t,
+  m, n, k: int,
+  alpha: ptr T, A: ptr T, lda: int,
+  B: ptr T; ldb: int;
+  beta: ptr T; C: ptr T; ldc: int): cublasStatus_t {.inline.}=
+
+  check cublasSetStream(defaultHandle, defaultStream)
+
+  when T is float32:
+    cublasSgemm(defaultHandle,
+                transa, transb,
+                m.cint, n.cint, k.cint,
+                alpha, A, lda.cint,
+                B, ldb.cint,
+                beta, C, ldc.cint)
+  elif T is float64:
+    cublasDgemm(defaultHandle,
+                transa, transb,
+                m.cint, n.cint, k.cint,
+                alpha, A, lda.cint,
+                B, ldb.cint,
+                beta, C, ldc.cint)

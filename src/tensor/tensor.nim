bafd0652ea8063fdbc1fb319ea61a7398d8cc9f8
Mamy Ratsimbazafy <mamy.ratsimbazafy_dev@gadz.org>
2017-10-08 17:56:47 +0200

    remove useless imports

--- src/tensor/tensor.nim
+++ src/tensor/tensor.nim
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sequtils, strutils, future, algorithm, nimblas, math, typetraits, macros, random
-
+import nimblas
 # Export OrderType (rowMajor, colMajor) from nimblas
 export OrderType
 

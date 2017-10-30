89744be6a17156b32cde61e2f1662ca8d97492cb
Mamy Ratsimbazafy <mratsim@users.noreply.github.com>
2017-10-30 14:34:27 +0100

    Revert "Fix OpenMP macOS huge slowdown (#134) + force SIMD in OMP section + Perf doc (PR #136)" (#137)
    This reverts commit 353d7e548fee9b1a6c252c4b46a7bc33f051b76f.

--- src/tensor/fallback/blas_l3_gemm_macro_kernel.nim
+++ src/tensor/fallback/blas_l3_gemm_macro_kernel.nim
@@ -28,15 +28,12 @@ proc gemm_macro_kernel[T](mc, nc, kc: int,
   let mod_mr = mc mod MR
   let mod_nr = nc mod NR
 
-  for j in `||`(0, np - 1, "simd"): # OpenMP loop
-    let nr =  if (j != np-1 or mod_nr == 0): NR
-              else: mod_nr
-
-    ## We can't declare private(i) in Nim OpenMP at the j level so we use a while loop instead of for loop.
-    var i = 0
-    while i < mp:
-      let mr =  if (i != mp-1 or mod_mr == 0): MR
-                else: mod_mr
+  for j in 0..<np:
+    let nr = if (j != np-1 or mod_nr == 0): NR
+             else: mod_nr
+    for i in 0 || (mp - 1): # OpenMP loop
+      let mr = if (i != mp-1 or mod_mr == 0): MR
+               else: mod_mr
 
       if (mr==MR and nr==NR):
         gemm_micro_kernel(kc, alpha,
@@ -61,5 +58,3 @@ proc gemm_macro_kernel[T](mc, nc, kc: int,
                 1, MR,
                 C, i*MR*incRowC+j*NR*incColC + offC,
                 incRowC, incColC)
-
-      inc i

6ddae7ecb5a260f380f1a009dec04ffbd8c43566
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2018-02-20 15:25:11 +0100

    CLBlast is only available for xenial + AMD SDK config from VexCL has broken permission

--- ci/opencl_amd_sdk.sh
+++ ci/opencl_amd_sdk.sh
@@ -2,53 +2,50 @@
 
 # Original script from https://github.com/gregvw/amd_sdk/
 
-export OPENCL_VENDOR_PATH=${AMDAPPSDKROOT}/etc/OpenCL/vendors
-export LD_LIBRARY_PATH=${AMDAPPSDKROOT}/lib/x86_64:${LD_LIBRARY_PATH}
-export CMAKE_LIBRARY_PATH=${AMDAPPSDKROOT}/lib/x86_64
+# Location from which get nonce and file name from
+URL="https://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
+URLDOWN="https://developer.amd.com/amd-license-agreement-appsdk/"
 
-if [ ! -e ${AMDAPPSDKROOT}/bin/x86_64/clinfo ]; then
-    # Location from which get nonce and file name from
-    URL="https://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
-    URLDOWN="https://developer.amd.com/amd-license-agreement-appsdk/"
+NONCE1_STRING='name="amd_developer_central_downloads_page_nonce"'
+FILE_STRING='name="f"'
+POSTID_STRING='name="post_id"'
+NONCE2_STRING='name="amd_developer_central_nonce"'
 
-    NONCE1_STRING='name="amd_developer_central_downloads_page_nonce"'
-    FILE_STRING='name="f"'
-    POSTID_STRING='name="post_id"'
-    NONCE2_STRING='name="amd_developer_central_nonce"'
+#For newest FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
+FORM=`wget --no-check-certificate -qO - $URL | sed -n '/download-5/,/64-bit/p'`
 
-    # This gets the second latest (2.9.1 ATM, latest is 3.0)
-    # For newest: FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
-    FORM=`wget -qO - $URL | sed -n '/download-5/,/64-bit/p'`
+# Get nonce from form
+NONCE1=`echo $FORM | awk -F ${NONCE1_STRING} '{print $2}'`
+NONCE1=`echo $NONCE1 | awk -F'"' '{print $2}'`
+echo $NONCE1
 
-    # Get nonce from form
-    NONCE1=`echo $FORM | awk -F ${NONCE1_STRING} '{print $2}'`
-    NONCE1=`echo $NONCE1 | awk -F'"' '{print $2}'`
-    echo $NONCE1
+# get the postid
+POSTID=`echo $FORM | awk -F ${POSTID_STRING} '{print $2}'`
+POSTID=`echo $POSTID | awk -F'"' '{print $2}'`
+echo $POSTID
 
-    # get the postid
-    POSTID=`echo $FORM | awk -F ${POSTID_STRING} '{print $2}'`
-    POSTID=`echo $POSTID | awk -F'"' '{print $2}'`
-    echo $POSTID
+# get file name
+FILE=`echo $FORM | awk -F ${FILE_STRING} '{print $2}'`
+FILE=`echo $FILE | awk -F'"' '{print $2}'`
+echo $FILE
 
-    # get file name
-    FILE=`echo $FORM | awk -F ${FILE_STRING} '{print $2}'`
-    FILE=`echo $FILE | awk -F'"' '{print $2}'`
-    echo $FILE
+FORM=`wget --no-check-certificate -qO - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
 
-    FORM=`wget -qO - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
+NONCE2=`echo $FORM | awk -F ${NONCE2_STRING} '{print $2}'`
+NONCE2=`echo $NONCE2 | awk -F'"' '{print $2}'`
+echo $NONCE2
 
-    NONCE2=`echo $FORM | awk -F ${NONCE2_STRING} '{print $2}'`
-    NONCE2=`echo $NONCE2 | awk -F'"' '{print $2}'`
-    echo $NONCE2
+wget --no-check-certificate --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
 
-    wget --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
+# unpacking and installing
+tar -xjf AMD-SDK.tar.bz2
+AMDAPPSDK=${HOME}/AMDAPPSDK
+export OPENCL_VENDOR_PATH=${AMDAPPSDK}/etc/OpenCL/vendors
+mkdir -p ${OPENCL_VENDOR_PATH}
+sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDK}
+echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd
+export LD_LIBRARY_PATH=${AMDAPPSDK}/lib/x86_64:${LD_LIBRARY_PATH}
+chmod +x ${AMDAPPSDK}/bin/x86_64/clinfo
 
-    # Unpack and install
-    tar -xjf AMD-SDK.tar.bz2;
-    mkdir -p ${OPENCL_VENDOR_PATH};
-    sh AMD-APP-SDK*.sh --tar -xf -C ${AMDAPPSDKROOT};
-    echo libamdocl64.so > ${OPENCL_VENDOR_PATH}/amdocl64.icd;
-    chmod +x ${AMDAPPSDKROOT}/bin/x86_64/clinfo;
-fi
-
-${AMDAPPSDKROOT}/bin/x86_64/clinfo
+# Checking OpenCL status
+${AMDAPPSDK}/bin/x86_64/clinfo

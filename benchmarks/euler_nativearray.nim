df56e25c45cf3b1c38c5284f8e52423847d78475
narimiran <miran.tuhtan+git@gmail.com>
2018-03-17 19:00:02 +0100

    faster native array version
    Precalculate `alpha / dz^2` to gain speed improvements.
    
    Array of arrays changed to seq of arrays:
    now it can handle 50000 time steps, as other versions.
    
    Changed `f` proc to modify existing `ke` array - gain a bit more speed.

--- benchmarks/euler_nativearray.nim
+++ benchmarks/euler_nativearray.nim
@@ -4,31 +4,34 @@ const
   dz = 0.01
   z = 100
   spaceSteps = int(z / dz)
-  timeSteps = 10000
+  timeSteps = 50000
   dt = 0.12 / timeSteps
   alpha = 2.0
   startingTemp = 30.0
   oscillations = 20.0
+  a_dz2 = alpha / dz^2
 
-
-proc f(T: array[spaceSteps, float]): array[spaceSteps-2, float] =
-  for i in 0 .. spaceSteps-3:
-    result[i] = alpha * (T[i] - 2.0 * T[i+1] + T[i+2]) / dz^2
-
-
-proc eulerSolve(Ts: var array[timeSteps, array[spaceSteps, float]]) =
-  for t in 0 ..< timeSteps-1:
-    let
-      k = f(Ts[t])
-    for i in 1 .. spaceSteps-2:
-      Ts[t+1][i] = Ts[t][i] + dt * k[i-1]
-    Ts[t+1][spaceSteps-1] = Ts[t+1][spaceSteps-2]
 
 let start = cpuTime()
+var ke: array[spaceSteps-2, float]
+
+
+proc f(k: var array[spaceSteps-2, float], T: array[spaceSteps, float]) =
+  for i in 0 .. spaceSteps-3:
+    k[i] = a_dz2 * (T[i] - 2.0 * T[i+1] + T[i+2])
+
+
+proc eulerSolve(Ts: var seq[array[spaceSteps, float]]) =
+  for t in 0 ..< timeSteps-1:
+    f(ke, Ts[t])
+    for i in 1 .. spaceSteps-2:
+      Ts[t+1][i] = Ts[t][i] + dt * ke[i-1]
+    Ts[t+1][spaceSteps-1] = Ts[t+1][spaceSteps-2]
+
 
 # TODO replace by a main proc
 # once https://github.com/nim-lang/Nim/issues/7349 is solved
-var Ts: array[timeSteps, array[spaceSteps, float]]
+var Ts = newSeq[array[spaceSteps, float]](timeSteps)
 
 for t in 0 ..< timeSteps:
   Ts[t][0] = startingTemp - oscillations * sin(2.0 * PI * t.float / timeSteps)
@@ -40,8 +43,8 @@ Ts.eulerSolve()
 let stop = cpuTime()
 
 let elapsed = stop - start
-echo &"Arraymancer Euler solve - time taken: {elapsed} seconds"
+echo &"Native array Euler solve - time taken: {elapsed} seconds"
 
-# Measurement on i5-5257U (Dual core mobile Broadwell 2.7Ghz)
-# Arraymancer Euler solve - time taken: 0.8557929999999999 seconds
-# 0.92s, 764.0Mb (as measured by xtime.rb)
+# Measurement on i7-970 (Hexa core 3.2Ghz)
+# Native array Euler solve - time taken: 2.732873 seconds
+# 2.87s, 3816.5Mb

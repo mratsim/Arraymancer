d44d8368c6dd91fb58eac8eca210e97750ab32e2
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2018-04-01 10:45:11 +0200

    Least square working for 1d, not for 2d

--- src/linear_algebra/least_square.nim
+++ src/linear_algebra/least_square.nim
@@ -8,7 +8,13 @@ import  ../tensor/tensor, ../tensor/backend/metadataArray,
         nimlapack, fenv, math
 
 
-proc least_square_solver*(a, b: Tensor[float32]): Tensor[float32] =
+proc least_square_solver*(a, b: Tensor[float32]):
+  tuple[
+    least_square_sol: Tensor[float32],
+    residuals:  Tensor[float32],
+    matrix_rank: int,
+    singular_values: Tensor[float32]
+  ] {.noInit.}=
 
   assert a.rank == 2 and b.rank in {1, 2} and a.shape[0] > 0 and b.shape[0] > 0
   # We need to copy the input as Lapack will destroy A and replace B with its result.
@@ -23,24 +29,28 @@ proc least_square_solver*(a, b: Tensor[float32]): Tensor[float32] =
   # Intel example for the Fortran API
   # https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/mkl_lapack_examples/dgelsd_ex.c.htm
 
-  var a  = a.clone(colMajor) # Lapack destroys the A matrix during solving
-  result = b.clone(colMajor) # Lapack replaces the B matrix by the result during solving
+  var a = a.clone(colMajor) # Lapack destroys the A matrix during solving
+  var b = b.clone(colMajor) # Lapack replaces the B matrix by the result during solving
+
+  let is1d = b.rank == 1
 
   var # Parameters
     m = a.shape[0].cint
     n = a.shape[1].cint
-    nrhs = if result.rank == 1: 1.cint
-           else: result.shape[1].cint
+    nrhs = if is1d: 1.cint
+           else: b.shape[1].cint
     # A is row-major so lda = m
     ldb = max(m, n).cint
 
   # Temporary sizes
   const smlsiz = 25.cint # equal to the maximum size of the subproblems at the bottom of the computation tree
   let
-    minmn = min(m,n)
+    minmn = min(m,n).int
     nlvl = max(0, (minmn.float32 / (smlsiz+1).float32).ln.cint + 1)
     # Bug in some Lapack, liwork must be determined manually: http://icl.cs.utk.edu/lapack-forum/archives/lapack/msg00899.html
     liwork = max(1, 3 * minmn * nlvl + 11 * minmn)
+
+  result.singular_values = newTensorUninit[float32](minmn) # will hold the singular values of A
 
   var # Temporary parameter values
     # Condition for a float to be considered 0
@@ -49,14 +59,13 @@ proc least_square_solver*(a, b: Tensor[float32]): Tensor[float32] =
     work = newSeqUninitialized[float32](lwork)
     iwork = newSeqUninitialized[cint](liwork)
     info, rank: cint
-    S = newSeqUninitialized[float32](minmn) # will hold the singular values of A
 
   # Solve the equations A*X = B
   sgelsd(
     m.addr, n.addr, nrhs.addr,
     a.get_data_ptr, m.addr, # lda
-    result.get_data_ptr, ldb.addr,
-    S[0].addr, rcond.addr, rank.addr,
+    b.get_data_ptr, ldb.addr,
+    result.singular_values[0].addr, rcond.addr, rank.addr,
     work[0].addr, lwork.addr,
     iwork[0].addr, info.addr
   )
@@ -68,3 +77,50 @@ proc least_square_solver*(a, b: Tensor[float32]): Tensor[float32] =
 
   if info < 0:
     raise newException(ValueError, "Illegal parameter in linear square solver gelsd: " & $(-info))
+
+  result.matrix_rank = rank.int # This is the matrix_rank not the tensor rank
+  if is1d:
+    result.least_square_sol = b[0 ..< n]
+    if rank == n and m > n:
+      result.residuals = (b[n .. _]).reduce_axis_inline(0):
+        x += y .^ 2f
+  else:
+    result.least_square_sol = b[0 ..< n, _]
+    if rank == n and m > n:
+      debugecho b
+      result.residuals = (b[n .. _, _]).reduce_axis_inline(0):
+        x += y .^ 2f
+
+
+when isMainModule:
+
+  # let a = [
+  #   [ 0.12f, -6.91, -3.33,  3.97],
+  #   [-8.19f,  2.22, -8.94,  3.33],
+  #   [ 7.69f, -5.12, -6.72, -2.74],
+  #   [-2.26f, -9.08, -4.40, -7.92],
+  #   [-4.71f,  9.96, -9.98, -3.20]
+  # ].toTensor
+
+  # let b = [
+  #   [ 7.30f,  1.33,  2.68, -9.62,  0.00],
+  #   [ 0.47f,  6.58, -1.71, -0.79,  0.00],
+  #   [-6.28f, -3.42,  3.46,  0.41,  0.00]
+  # ].toTensor
+
+  # let r = least_square_solver(a, b)
+
+  # echo r
+
+  let A = [
+    [0f, 1f],
+    [1f, 1f],
+    [2f, 1f],
+    [3f, 1f]
+  ].toTensor
+
+  let y = [-1f, 0.2, 0.9, 2.1].toTensor
+
+  let s = least_square_solver(A, y)
+
+  echo s

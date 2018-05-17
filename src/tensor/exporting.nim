cdf1bf18fa81eb4702a9e2f9b589ce7574135c08
Fabian Keller <github.100.fkeller@spamgourmet.com>
2018-05-17 23:16:28 +0200

    added higher dimensional toSeq exports

--- src/tensor/exporting.nim
+++ src/tensor/exporting.nim
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import ./data_structure
+import sequtils
 
 proc toRawSeq*[T](t:Tensor[T]): seq[T] {.noSideEffect.} =
   ## Convert a tensor to the raw sequence of data.
@@ -38,3 +39,65 @@ proc export_tensor*[T](t: Tensor[T]):
   result.shape = contig_t.shape
   result.strides = contig_t.strides
   result.data = contig_t.data
+
+proc toSeq2D*[T](t: Tensor[T]): seq[seq[T]] =
+  ## Exports a rank-2 tensor to a 2D sequence.
+  if t.rank != 2:
+    raise newException(ValueError, "Tensor must be of rank 2")
+  result = newSeqWith(
+    t.shape[0], newSeq[T](t.shape[1])
+  )
+  for i in 0 ..< t.shape[0]:
+    for j in 0 ..< t.shape[1]:
+      result[i][j] = t[i, j]
+
+proc toSeq3D*[T](t: Tensor[T]): seq[seq[seq[T]]] =
+  ## Exports a rank-3 tensor to a 3D sequence.
+  if t.rank != 3:
+    raise newException(ValueError, "Tensor must be of rank 3")
+  result = newSeqWith(
+    t.shape[0], newSeqWith(
+      t.shape[1], newSeq[T](t.shape[2])
+    )
+  )
+  for i in 0 ..< t.shape[0]:
+    for j in 0 ..< t.shape[1]:
+      for k in 0 ..< t.shape[2]:
+        result[i][j][k] = t[i, j, k]
+
+proc toSeq4D*[T](t: Tensor[T]): seq[seq[seq[seq[T]]]] =
+  ## Exports a rank-4 tensor to a 4D sequence.
+  if t.rank != 4:
+    raise newException(ValueError, "Tensor must be of rank 4")
+  result = newSeqWith(
+    t.shape[0], newSeqWith(
+      t.shape[1], newSeqWith(
+        t.shape[2], newSeq[T](t.shape[3])
+      )
+    )
+  )
+  for i in 0 ..< t.shape[0]:
+    for j in 0 ..< t.shape[1]:
+      for k in 0 ..< t.shape[2]:
+        for l in 0 ..< t.shape[3]:
+          result[i][j][k][l] = t[i, j, k, l]
+
+proc toSeq5D*[T](t: Tensor[T]): seq[seq[seq[seq[seq[T]]]]] =
+  ## Exports a rank-5 tensor to a 5D sequence.
+  if t.rank != 5:
+    raise newException(ValueError, "Tensor must be of rank 5")
+  result = newSeqWith(
+    t.shape[0], newSeqWith(
+      t.shape[1], newSeqWith(
+        t.shape[2], newSeqWith(
+          t.shape[3], newSeq[T](t.shape[4])
+        )
+      )
+    )
+  )
+  for i in 0 ..< t.shape[0]:
+    for j in 0 ..< t.shape[1]:
+      for k in 0 ..< t.shape[2]:
+        for l in 0 ..< t.shape[3]:
+          for m in 0 ..< t.shape[4]:
+            result[i][j][k][l][m] = t[i, j, k, l, m]
---
+++ tests/tensor/test_exporting.nim
@@ -0,0 +1,44 @@
+# Copyright 2017 the Arraymancer contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ../../src/arraymancer
+import unittest, sequtils
+
+suite "Exporting a tensor":
+  test "Raw sequence exporting":
+    let t = toSeq(1..6).toTensor().reshape(2, 3)
+    check t.toRawSeq == toSeq(1 .. 6)
+
+  test "Nested sequence exporting":
+    block:
+      let s = @[@[1, 2, 3], @[4, 5, 6]]
+      check s.toTensor().toSeq2D() == s
+    block:
+      let t = toSeq(1..24).toTensor().reshape(2, 3, 4)
+      check t.toSeq3D().len == 2
+      check t.toSeq3D()[0].len == 3
+      check t.toSeq3D()[0][0].len == 4
+    block:
+      let t = toSeq(1..120).toTensor().reshape(2, 3, 4, 5)
+      check t.toSeq4D().len == 2
+      check t.toSeq4D()[0].len == 3
+      check t.toSeq4D()[0][0].len == 4
+      check t.toSeq4D()[0][0][0].len == 5
+    block:
+      let t = toSeq(1..720).toTensor().reshape(2, 3, 4, 5, 6)
+      check t.toSeq5D().len == 2
+      check t.toSeq5D()[0].len == 3
+      check t.toSeq5D()[0][0].len == 4
+      check t.toSeq5D()[0][0][0].len == 5
+      check t.toSeq5D()[0][0][0][0].len == 6

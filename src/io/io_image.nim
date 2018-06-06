26eae2a67146657732e3dd5508b5bb9a735574b1
Mamy Ratsimbazafy <mratsim@users.noreply.github.com>
2018-06-06 22:42:29 +0200

    Implement Image IO (#244)
    * Add image reading
    
    * Remove TODO comment, added as issue #245
    
    * Add loading image from buffer and writing to disk
    
    * Fix nimble import

---
+++ src/io/io_image.nim
@@ -0,0 +1,87 @@
+# Copyright (c) 2018 Mamy André-Ratsimbazafy and the Arraymancer contributors
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+import
+  stb_image/read,
+  stb_image/write,
+  ../tensor/tensor
+
+func whc_to_chw[T](img: Tensor[T]): Tensor[T] {.inline.}=
+  ## Convert image from Width x Height x Channel convention
+  ## to the Channel x Height x Width convention.
+  img.permute(2, 1, 0)
+
+func chw_to_whc[T](img: Tensor[T]): Tensor[T] {.inline.}=
+  ## Convert image from Channel x Height x Width convention
+  ## to the Width x Height x Channel convention.
+  img.permute(2, 1, 0)
+
+proc read_image*(filepath: string): Tensor[uint8] =
+  ## Read an image file and loads it into a Tensor[uint8] of shape
+  ## Channel x Height x Width. Channel is 1 for greyscale, 3 for RGB.
+  ##
+  ## Supports JPEG, PNG, TGA, BMP, PSD, GIF, HDR, PIC, PNM
+  ## See stb_image https://github.com/nothings/stb/blob/master/stb_image.h
+  ##
+  ## Usage example with conversion to [0..1] float:
+  ## .. code:: nim
+  ##   let raw_img = read_image('path/to/image.png')
+  ##   let img = raw_img.map_inline:
+  ##     x.float32 / 255.0
+
+  var width, height, channels: int
+  let desired_channels = 0 # Channel autodetection
+
+  let raw_pixels = load(filepath, width, height, channels, desired_channels)
+  result = raw_pixels.toTensor.reshape(width, height, channels).whc_to_chw
+
+proc read_image*(buffer: seq[byte]): Tensor[uint8] =
+  ## Read an image from a buffer and loads it into a Tensor[uint8] of shape
+  ## Channel x Height x Width. Channel is 1 for greyscale, 3 for RGB.
+  ##
+  ## Supports JPEG, PNG, TGA, BMP, PSD, GIF, HDR, PIC, PNM
+  ## See stb_image https://github.com/nothings/stb/blob/master/stb_image.h
+  ##
+
+  # TODO: ideally this should also accept pointer + length
+  # but nim-stb_image only accept seq[bytes] (and convert it to pointer + length internally)
+
+  var width, height, channels: int
+  let desired_channels = 0 # Channel autodetection
+
+  let raw_pixels = load_from_memory(buffer, width, height, channels, desired_channels)
+  result = raw_pixels.toTensor.reshape(width, height, channels).whc_to_chw
+
+
+template gen_write_image(proc_name: untyped): untyped {.dirty.}=
+
+  proc proc_name*(img: Tensor[uint8], filepath: string) =
+    ## Create an image file from a tensor
+
+    var success = false
+    let
+      img = img.chw_to_whc.asContiguous(rowMajor, force = true)
+      w = img.shape[0]
+      h = img.shape[1]
+      c = img.shape[2]
+    success = proc_name(filepath, w, h, c, img.storage.Fdata)
+
+    doAssert success
+
+gen_write_image(write_png)
+gen_write_image(write_bmp)
+gen_write_image(write_tga)
+
+proc write_jpg*(img: Tensor[uint8], filepath: string, quality = 100) =
+  ## Create a jpeg image file from a tensor
+
+  var success = false
+  let
+    img = img.chw_to_whc.asContiguous(rowMajor, force = true)
+    w = img.shape[0]
+    h = img.shape[1]
+    c = img.shape[2]
+  success = write_jpg(filepath, w, h, c, img.storage.Fdata, quality)
+
+  doAssert success
---
+++ tests/io/test_image.nim
@@ -0,0 +1,28 @@
+# Copyright (c) 2018 Mamy André-Ratsimbazafy and the Arraymancer contributors
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+import ../../src/arraymancer
+import unittest, os
+
+suite "[IO] Reading and writing images":
+  const
+    imgpath = "./tests/io/images/nim_in_action_cover.jpg"
+    outpng = "./build/test_img_out.png"
+    outjpg = "./build/test_img_out.jpg"
+
+  test "[IO] Reading images":
+    let img = read_image(imgpath)
+
+    let channels = 3
+    let width = 767
+    let height = 964
+
+    check: img.shape == [channels, height, width].toMetadataArray
+
+  test "[IO] Writing images":
+    let img = read_image(imgpath)
+
+    img.write_png(outpng)
+    img.write_jpg(outjpg, quality = 70)
+

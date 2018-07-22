1af8337da37bd821174a75a85bfd7ada278b3ec5
Vindaar <basti90@gmail.com>
2018-07-22 17:50:30 +0200

    guard `io_hdf5` by `canImport` template
    There might be a better way to check whether the user has the
    `nimhdf5` library installed. Check.

--- src/io/io.nim
+++ src/io/io.nim
@@ -2,6 +2,16 @@
 # Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
 # This file may not be copied, modified, or distributed except according to those terms.
 
-import ./io_csv, ./io_npy, ./io_image, ./io_hdf5
+template canImport(x: untyped): bool =
+  compiles:
+    import x
 
-export io_csv, io_npy, io_image, io_hdf5
+import ./io_csv, ./io_npy, ./io_image
+
+export io_csv, io_npy, io_image
+
+when canImport(nimhdf5):
+  # only provide the hdf5 interface, if the hdf5 library is
+  # installed to avoid making `nimhdf5` a dependency
+  import ./io_hdf5
+  export io_hdf5

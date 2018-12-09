7810112d8ab338672da0c8ee4234bae953133e6e
mratsim <mamy_github@numforge.co>
2018-12-09 17:13:34 +0100

    Fix and export embedding high level layer

--- src/nn/nn.nim
+++ src/nn/nn.nim
@@ -12,17 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import  ./activation/sigmoid,
-        ./activation/relu,
-        ./activation/tanh,
-        ./layers/linear,
-        ./layers/conv2D,
-        ./layers/maxpool2D,
-        ./layers/gru,
+import  ./activation/[sigmoid, relu, tanh],
+        ./layers/[linear, conv2D, maxpool2D, gru, embedding],
         ./loss/cross_entropy_losses,
         ./loss/mean_square_error_loss,
         ./optimizers/optimizers
 
-export sigmoid, relu, tanh
-export linear, conv2D, maxpool2d, gru, cross_entropy_losses, mean_square_error_loss
-export optimizers
+export  sigmoid, relu, tanh,
+        linear, conv2D, maxpool2d, gru, embedding,
+        cross_entropy_losses, mean_square_error_loss,
+        optimizers

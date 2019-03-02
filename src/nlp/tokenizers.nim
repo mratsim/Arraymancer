1692e8cc15ad0cb55470b2bb02c6e97257193ac2
Alexander Johnson <xander@splunk.com>
2019-03-02 13:27:42 -0800

    2019

--- src/nlp/tokenizers.nim
+++ src/nlp/tokenizers.nim
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 the Arraymancer contributors
+# Copyright (c) 2019 the Arraymancer contributors
 # Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
 # This file may not be copied, modified, or distributed except according to those terms.
 

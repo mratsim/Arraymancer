cb42e51c3e50871790ee20605c0bab7233952c8e
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-09-22 22:33:25 +0200

    least squares follows matrix decompositions code organization

--- src/linear_algebra/helpers/decomposition_lapack.nim
+++ src/linear_algebra/helpers/decomposition_lapack.nim
@@ -3,7 +3,8 @@
 # This file may not be copied, modified, or distributed except according to those terms.
 
 import
-  macros, nimlapack,
+  nimlapack,
+  ./overload,
   ../../private/sequninit,
   ../../tensor/tensor,
   ../../tensor/private/p_init_cpu # TODO: can't call newTensorUninit with optional colMajor, varargs breaks it
@@ -20,37 +21,6 @@ import
 #       - https://github.com/nim-lang/Nim/issues/5905
 #       - https://github.com/nim-lang/Nim/issues/7308
 #       - https://github.com/nim-lang/Nim/issues/11797
-
-# Alias / overload generator
-# --------------------------------------------------------------------------------------
-macro overload(overloaded_name: untyped, lapack_name: typed{nkSym}): untyped =
-  let impl = lapack_name.getImpl()
-  impl.expectKind {nnkProcDef, nnkFuncDef}
-
-  # We can't just `result[0] = overloaded_name`
-  # as libName (lapack library) is not defined in this scope
-
-  var
-    params = @[newEmptyNode()] # No return value for all Lapack proc
-    body = newCall(lapack_name)
-
-  impl[3].expectKind nnkFormalParams
-  for idx in 1 ..< impl[3].len:
-    # Skip arg 0, the return type which is always empty
-    params.add impl[3][idx]
-    body.add impl[3][idx][0]
-
-  result = newProc(
-    name = overloaded_name,
-    params = params,
-    body = body,
-    pragmas = nnkPragma.newTree(ident"inline")
-  )
-
-  when false:
-    # View proc signature.
-    # Some procs like syevr have over 20 parameters
-    echo result.toStrLit
 
 # SVD and Eigenvalues/eigenvectors decomposition
 # --------------------------------------------------------------------------------------

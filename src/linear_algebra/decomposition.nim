5e0a65759b94c024e89668f93d0fac9a088a3437
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-09-27 20:32:20 +0200

    proc for the init colmajor workaround

--- src/linear_algebra/decomposition.nim
+++ src/linear_algebra/decomposition.nim
@@ -7,8 +7,14 @@ import
   ../private/sequninit,
   ./helpers/[decomposition_lapack, triangular, auxiliary_lapack]
 
+# Helpers
+# -------------------------------------------
+
 template `^^`(s, i: untyped): untyped =
   (when i is BackwardsIndex: s.shape[0] - int(i) else: int(i))
+
+# Full decompositions
+# -------------------------------------------
 
 proc symeig*[T: SomeFloat](a: Tensor[T], eigenvectors = false): tuple[eigenval, eigenvec: Tensor[T]] {.inline.}=
   ## Compute the eigenvalues and eigen vectors of a symmetric matrix
@@ -59,6 +65,9 @@ proc qr*[T: SomeFloat](a: Tensor[T]): tuple[Q, R: Tensor[T]] =
   ##   - Q orthonormal matrix of shape [M, K]
   ##   - R upper-triangular matrix of shape [K, N]
 
+  # Checking correctness:
+  # - https://software.intel.com/en-us/articles/checking-correctness-of-lapack-svd-eigenvalue-and-one-sided-decomposition-routines
+
   let k = min(a.shape[0], a.shape[1])
 
   var scratchspace: seq[T]
@@ -71,44 +80,6 @@ proc qr*[T: SomeFloat](a: Tensor[T]): tuple[Q, R: Tensor[T]] =
 
   orgqr(result.Q, tau, scratchspace)
   result.Q = result.Q[_, 0..<k]
-
-proc svd*[T: SomeFloat](a: Tensor[T]): tuple[U, S, Vh: Tensor[T]] =
-  ## Compute the Singular Value Decomposition of an input matrix ``a``
-  ## Decomposition is done through recursive divide & conquer.
-  ##
-  ## Input:
-  ##   - ``a``, matrix of shape [M, N]
-  ##
-  ## Returns:
-  ##   with K = min(M, N)
-  ##   - ``U``: Unitary matrix of shape [M, K] with left singular vectors as columns
-  ##   - ``S``: Singular values diagonal of length K in decreasing order
-  ##   - ``Vh``: Unitary matrix of shape [K, N] with right singular vectors as rows
-  ##
-  ## SVD solves the equation:
-  ## A = U S V.h
-  ##
-  ## - with S being a diagonal matrix of singular values
-  ## - with V being the right singular vectors and
-  ##   V.h being the hermitian conjugate of V
-  ##   for real matrices, this is equivalent to V.t (transpose)
-  ##
-  ## ⚠️: Input must not contain NaN
-  ##
-  ## Compared to Numpy svd procedure, we default to "full_matrices = false".
-  ##
-  ## Exception:
-  ##   - This can throw if the algorithm did not converge.
-
-  # Numpy default to full_matrices is
-  # - confusing for docs
-  # - hurts reconstruction
-  # - often not used (for example for PCA/randomized PCA)
-  # - memory inefficient
-  # thread: https://mail.python.org/pipermail/numpy-discussion/2011-January/054685.html
-  var a = a.clone(colMajor) # gesdd destroys its input
-  var scratchspace: seq[T]
-  gesdd(a, result.U, result.S, result.Vh, scratchspace)
 
 proc lu_permuted*[T: SomeFloat](a: Tensor[T]): tuple[PL, U: Tensor[T]] =
   ## Compute the pivoted LU decomposition of an input matrix ``a``.
@@ -140,3 +111,49 @@ proc lu_permuted*[T: SomeFloat](a: Tensor[T]): tuple[PL, U: Tensor[T]] =
   result.PL = result.PL[_, 0..<k]
   tril_unit_diag_mut(result.PL)
   laswp(result.PL, pivot_indices, pivot_from = -1)
+
+proc svd*[T: SomeFloat](a: Tensor[T]): tuple[U, S, Vh: Tensor[T]] =
+  ## Compute the Singular Value Decomposition of an input matrix ``a``
+  ## Decomposition is done through recursive divide & conquer.
+  ##
+  ## Input:
+  ##   - ``a``, matrix of shape [M, N]
+  ##
+  ## Returns:
+  ##   with K = min(M, N)
+  ##   - ``U``: Unitary matrix of shape [M, K] with left singular vectors as columns
+  ##   - ``S``: Singular values diagonal of length K in decreasing order
+  ##   - ``Vh``: Unitary matrix of shape [K, N] with right singular vectors as rows
+  ##
+  ## SVD solves the equation:
+  ## A = U S V.h
+  ##
+  ## - with S being a diagonal matrix of singular values
+  ## - with V being the right singular vectors and
+  ##   V.h being the hermitian conjugate of V
+  ##   for real matrices, this is equivalent to V.t (transpose)
+  ##
+  ## ⚠️: Input must not contain NaN
+  ##
+  ## Compared to Numpy svd procedure, we default to "full_matrices = false".
+  ##
+  ## Exception:
+  ##   - This can throw if the algorithm did not converge.
+
+  # Checking correctness:
+  # - https://software.intel.com/en-us/articles/checking-correctness-of-lapack-svd-eigenvalue-and-one-sided-decomposition-routines
+
+  # Numpy default to full_matrices is
+  # - confusing for docs
+  # - hurts reconstruction
+  # - often not used (for example for PCA/randomized PCA)
+  # - memory inefficient
+  # thread: https://mail.python.org/pipermail/numpy-discussion/2011-January/054685.html
+  var a = a.clone(colMajor) # gesdd destroys its input
+  var scratchspace: seq[T]
+  gesdd(a, result.U, result.S, result.Vh, scratchspace)
+
+# Randomized decompositions
+# -------------------------------------------
+
+

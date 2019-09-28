bf2a431fea0ea9783bc5ebee11bd85f1c0754258
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-09-28 02:13:39 +0200

    Add Hilbert matrix for testing decompositions

--- src/linear_algebra/linear_algebra.nim
+++ src/linear_algebra/linear_algebra.nim
@@ -2,6 +2,6 @@
 # Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
 # This file may not be copied, modified, or distributed except according to those terms.
 
-import ./least_squares, ./decomposition
+import ./least_squares, ./decomposition, ./special_matrices
 
-export least_squares, decomposition
+export least_squares, decomposition, special_matrices

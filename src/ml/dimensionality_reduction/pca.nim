368a19ee78402dc129baac698560fb4bfffe66ad
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-09-30 19:47:23 +0200

    The loading terminology is a mess: https://stats.stackexchange.com/questions/143905/loadings-vs-eigenvectors-in-pca-when-to-use-one-or-another, https://stats.stackexchange.com/questions/123063/is-there-any-good-reason-to-use-pca-instead-of-efa-also-can-pca-be-a-substitut/123136#123136

--- src/ml/dimensionality_reduction/pca.nim
+++ src/ml/dimensionality_reduction/pca.nim
@@ -7,20 +7,21 @@ import
 
 
 proc pca*[T: SomeFloat](
-       x: Tensor[T], n_components = 2, center: static bool = true,
+       X: Tensor[T], n_components = 2, center: static bool = true,
        n_oversamples = 5,
        n_power_iters = 2
-      ): tuple[scores: Tensor[T], loadings: Tensor[T]] {.noInit.} =
+      ): tuple[projected: Tensor[T], components: Tensor[T]] {.noInit.} =
   ## Principal Component Analysis (PCA)
   ##
   ## Project the input data ``X`` of shape [Observations, Features] into a new coordinate system
-  ## where axis are in descending order of explained variance of the original ``X`` data
+  ## where axes (principal components) are in descending order of explained variance of the original ``X`` data
+  ## i.e. the first axis explains most of the variance.
   ##
-  ## Projection is called ``scores`` and the project values are linearly uncorrelated.
-  ## The rotation matrix ``loadings`` can be used to project new observations onto the same base:
-  ##   X' * loadings, with X' of shape [Observations', Features]
+  ## The rotated ``components`` cmatrix can be used to project new observations onto the same base:
+  ##   X' * loadings, with X' of shape [Observations', Features].
+  ##   X' must be mean centered
   ## Its transposed can be use to reconstruct the original X:
-  ##   X ~= scores * loadings.transpose()
+  ##   X ~= projected * components.transpose()
   ##
   ## PCA requires:
   ##   - mean-centered features. This procedure does the centering by default.
@@ -37,16 +38,16 @@ proc pca*[T: SomeFloat](
   ##   - The number of components to keep (default 2D for 2D projection)
   ##
   ## Returns:
-  ##   - A tuple of PCA scores (projected matrix) and loadings (rotation matrix):
-  ##     scores: a matrix of shape [Nb of observations, Nb of components] in descending order of explained variance
-  ##     loadings: a matrix of shape [Nb of features, Nb of components] in descending order of explained variance
-  assert x.rank == 2
+  ##   - A tuple of PCA projected matrix and principal components matrix:
+  ##     projected: a matrix of shape [Nb of observations, Nb of components] in descending order of explained variance
+  ##     components: a matrix of shape [Nb of features, Nb of components] to project new data on the same orthogonal basis
+  assert X.rank == 2
 
   when center:
     # TODO: When we center, we could do in-place randomized SVD and save memory from cloning x
     #       but that only happen when the number of components is within 25% of [Observations, Features]
-    let x = x .- x.mean(axis=0)
+    let X = X .- X.mean(axis=0)
 
-  let (U, S, Vh) = svd_randomized(x, n_components)
-  result.scores = U .* S.unsqueeze(0)
-  result.loadings = Vh
+  let (U, S, Vh) = svd_randomized(X, n_components)
+  result.components = Vh.transpose
+  result.projected = U .* S.unsqueeze(0) # S sparse diagonal, equivalent to multiplying by a dense diagonal matrix

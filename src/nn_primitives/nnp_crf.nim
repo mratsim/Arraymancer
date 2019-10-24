1db105e2c0124d72c02c2c075c5b9a5a2503ebb8
Sloane Simmons <sloanes.k@gmail.com>
2019-10-24 02:01:44 -0500

    Start on crf_forward
    Implementation of forward pass underway, starting with scores (non
    normalized log prob with emission + transition components).

--- src/nn_primitives/nnp_crf.nim
+++ src/nn_primitives/nnp_crf.nim
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import strformat
+
 import ../tensor/tensor,
         math
 
@@ -23,18 +25,80 @@ type Idx = SomeInteger
 
 
 proc compute_scores[T](
-  input: Tensor[T],
-  mask: Tensor[T],
-  transitions: Tensor[T],
-  tags: Tensor[Idx]
-): Tensor[T] =
+  result: var Tensor[T],  # (B, ) - not nil
+  input: Tensor[T],       # (T, B, num_tags)
+  mask: Tensor[T],        # (T, B)
+  transitions: Tensor[T], # (num_tags + 2, num_tags + 2)
+  tags: Tensor[Idx],      # (T, B)
+  timesteps, batch_size, hidden_dim: int,
+  bos_tag, eos_tag: Idx
+) =
   ## Computes the un-normalized log probabilities (combination of emissions and
   ## transition scores at each timestep).
   ##
   ## Returns:
   ##  - A Tensor[T] of non-normalized emission scores of shape [batch_size, ]
-  discard
 
+  echo (timesteps, batch_size, hidden_dim)
+  echo input.shape
+
+  # Transitions from bos_tag -> tag at time = 0 for all batches
+  var transition_scores = index_select(transitions[bos_tag, _], axis = 1,
+                                      indices = tags[0, _].squeeze()).squeeze()
+
+  when compileOption("boundChecks"):
+    doAssert result.shape == [batch_size], "Result should be of shape" &
+      fmt" {batch_size} but got {result.shape}"
+    doAssert transition_scores.shape == [batch_size], "Transition scores" &
+      fmt" should be of shape {batch_size} but got {transition_scores.shape}"
+
+  # Emission scores for tag at t = 0 for all in batch
+  # FIXME: This is giving a value of wrong shape below
+  var emission_scores = input[0, _, _]
+                          .squeeze()
+                          .index_select(axis = 1, indices = tags[0, _].squeeze())
+                          .squeeze(axis=1)
+
+  when compileOption("boundChecks"): 
+    doAssert emission_scores.shape == [batch_size], "Emission scores should" &
+      fmt" be of shape {batch_size} but got {emission_scores.shape}"
+
+  emission_scores .*= mask[0, _].squeeze()
+
+  result += transition_scores + emission_scores
+
+  # TODO: Optimize?
+  for i in 1 ..< timesteps:
+    let 
+      old_tags = tags[i - 1, _].squeeze(1)
+      new_tags = tags[i, _].squeeze(1)
+
+      old_mask = mask[i, _].squeeze()
+      new_mask = mask[i + 1, _].squeeze()
+
+    # New emission scores
+    input[i, _, _].squeeze().index_select(axis=1, tags[i, _].squeeze(),
+                                          result = emission_scores)
+
+    # New transition scores
+    # This is applying transtion from old -> new tag across batch
+    # Unoptimized version:
+    # for j in 0 .. batch_size:
+    #   transition_scores[j] = transitions[old_tags[j], new_tags[j]]
+    transition_scores.apply3_inline(old_tags, new_tags):
+      transitions[y, z]
+
+    result += transition_scores * new_mask + emission_scores * old_mask
+  
+  # Assume that masked when == 0
+  let last_time_inds = (mask.sum(axis=0).squeeze() .- 1).astype(int)
+  let last_tags = tags.index_select(axis=0, indices=last_time_inds).squeeze()
+  # let last_transitions = transitions[_, eos_tag].squeeze()
+
+  # Set transition scores to last_real_tag -> EOS_TAG across batch
+  transitions[_, eos_tag].squeeze().index_select(axis=0, indices=last_tags, result=transition_scores)
+  
+  result += transition_scores
 
 proc compute_log_partition_function[T](): Tensor[T] =
   ## Compute the partition function by using the forward algorithm to avoid
@@ -42,15 +106,17 @@ proc compute_log_partition_function[T](): Tensor[T] =
   ## configurations.
   discard
 
-
 proc crf_forward*[T: SomeFloat](
   result: var Tensor[T],
   input: Tensor[T],
   mask: Tensor[T],
   transitions: Tensor[T],
   tags: Tensor[Idx],
-  reduce: bool
+  timesteps, batch_size, hidden_dim: int,
+  bos_tag, eos_tag: Idx
 ) =
   ## Computes the log likelihood of input given transitions (emissions) matrix.
   ## Loss should be *negative* log likelihood.
-  discard
+  result = zeros[T](batch_size)
+  result.compute_scores(input, mask, transitions, tags, timesteps, batch_size,
+                        hidden_dim, bos_tag, eos_tag)

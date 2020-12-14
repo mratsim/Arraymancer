81cebcf867e913cb8e095b9e76f5cc9b2993ae21
Clonkk <sidger@zoho.com>
2020-12-14 19:08:08 +0100

    added csize_t for Nim < 1.2.0

--- src/arraymancer.nim
+++ src/arraymancer.nim
@@ -39,7 +39,12 @@ export  tensor,
         einsum
 
 when (NimMajor, NimMinor, NimPatch) < (1, 4, 0):
+  # IndexDefect was introduced in 1.4.0
   type IndexDefect* = IndexError
+
+when (NimMajor, NimMinor, NimPatch) < (1, 2, 0):
+  # csize_t was introduced in 1.2.0
+  type csize_t* {.importc: "size_t", nodecl.} = uint
 
 when not defined(no_lapack):
   # The ml module also does not export everything is LAPACK is not available

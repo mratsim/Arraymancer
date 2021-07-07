d6c0a970058acd6991b520919be7ebbc65e19cb5
Vindaar <basti90@gmail.com>
2021-07-08 01:03:14 +0200

    use tensor argsort instead of custom seq argsort

--- src/arraymancer/spatial/neighbors.nim
+++ src/arraymancer/spatial/neighbors.nim
@@ -4,17 +4,11 @@ import sequtils
 import ../tensor
 import ./distance
 
-proc argsort[T: SomeFloat](x: seq[T]): seq[int] =
-  result = toSeq(0..high(x))
-  sort(result, proc(i, j:int): int = cmp(x[i], x[j]))
-
-
 proc neighborsFromDistanceMatrix[T](X: Tensor[T], eps: float): seq[int] =
-  for j in argsort(toSeq(X)):
+  for j in argsort(X, toCopy = true):
     if X[j, 0] > eps:
       break
     result.add(j)
-
 
 proc simpleNearestNeighbors*[T](X: Tensor[T], eps:float, metric: PairwiseDist): seq[seq[int]] =
   let distances = pairwise(metric, X, X)

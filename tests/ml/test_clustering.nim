46e1fa474548bbb185e4913b8092a10d7f523d91
Vindaar <basti90@gmail.com>
2021-07-10 14:13:42 +0200

    [tests] add test case for DBSCAN, another for kmeans
    And mini improvement for `neighbors`

--- tests/ml/test_clustering.nim
+++ tests/ml/test_clustering.nim
@@ -5,7 +5,28 @@
 import ../../src/arraymancer, ../testutils
 import
   unittest,
-  math
+  math,
+  random,
+  sequtils
+
+proc generateRandomClusters(): (Tensor[float], seq[int]) =
+  ## Generate 3 random clusters around 3 different centers with
+  ## different radii. Also returns the correct labels for each point.
+  randomize(42)
+  var xs = newSeq[float]()
+  var ys = newSeq[float]()
+  let centers = [0.1, 0.32, 0.7]
+  let radii = [0.15,  0.1, 0.2]
+  var labels = newSeq[int]()
+  for i in 0 ..< 1000:
+    let centerIdx = rand(2)
+    labels.add centerIdx
+    let radius = rand(radii[centerIdx])
+    let angle = rand(2 * PI)
+    xs.add centers[centerIdx] + radius * cos(angle)
+    ys.add centers[centerIdx] + radius * sin(angle)
+  result = (stack([xs.toTensor, ys.toTensor]).transpose,
+            labels)
 
 proc main() =
   suite "[ML] Clustering":
@@ -15,6 +36,8 @@ proc main() =
 
     # Keep copy of data to check for modification
     let cleanData = data.clone()
+
+    let (randomClusters, randomLabels) = generateRandomClusters()
 
     test "KMeans - KMeans++ Centroids":
 
@@ -49,6 +72,40 @@ proc main() =
         data == cleanData
         round(inertia, 4) == 97.0019
 
+    test "Kmeans - More random clusters":
+      var
+        (labels, centroids, inertia) = randomClusters.clone.kmeans(n_clusters=3, random=false)
+      # NOTE: the labels as they are assigned to the clusters are different than the random
+      # labels we start with (but the clusters themselves match). Thus we will have 2 arrays
+      # one for each set of labels that we fill with the correct indices (counted from 0).
+      # Then we can use the labels to get the correct "index" and compare if they match.
+      var lLabs = [-1, -1, -1]
+      var rLabs = [-1, -1, -1]
+      var curIdx = 0
+      for (l, r) in zip(labels.toRawSeq, randomLabels):
+        if lLabs[l] == -1:
+          lLabs[l] = curIdx
+          doAssert rLabs[r] == -1
+          rLabs[r] = curIdx
+          inc curIdx
+        check lLabs[l] == rLabs[r]
+
+    test "DBSCAN  - Centroids":
+      let res = randomClusters.dbscan(eps = 0.05, minSamples = 10, Euclidean)
+
+      check res.deduplicate.len == 4 # we have a few pixels not within a cluster
+      # to get invalid id `notInClusters` array:
+      when false:
+        for i, r in res:
+          if r == -1:
+            echo i
+      check res.len == randomLabels.len
+      let notInCluster = [200, 246, 631, 696]
+      for i in 0 ..< res.len:
+        if i in notInCluster:
+          check res[i] == -1
+        else:
+          check res[i] == randomLabels[i]
 
 main()
 GC_fullCollect()

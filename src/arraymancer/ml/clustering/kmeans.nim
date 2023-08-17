fda5c00bb82831445456057ce02aaff13a2dcb50
Vindaar <basti90@gmail.com>
2023-08-17 12:14:28 +0200

    fix import of math, random, table sin `kmeans`

--- src/arraymancer/ml/clustering/kmeans.nim
+++ src/arraymancer/ml/clustering/kmeans.nim
@@ -1,7 +1,7 @@
 # Copyright (c) 2018 Mamy André-Ratsimbazafy and the Arraymancer contributors
 # Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
 # This file may not be copied, modified, or distributed except according to those terms.
-import math, random, tables
+import std / [math, random, tables]
 
 import ../../tensor
 import ../../spatial/distances

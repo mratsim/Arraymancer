1936506f63648ca2f04304679867c76ee3bb0ff4
Angel Ezquerra <angel_ezquerra@keysight.com>
2023-10-07 00:27:04 +0200

    Fix "use `uint`; ByteAddress is deprecated" warning

--- src/arraymancer/laser/private/memory.nim
+++ src/arraymancer/laser/private/memory.nim
@@ -9,12 +9,12 @@ func align_raw_data*(T: typedesc, p: pointer): ptr UncheckedArray[T] =
   static: assert T is KnownSupportsCopyMem
   withCompilerOptimHints()
 
-  let address = cast[ByteAddress](p)
+  let address = cast[uint](p)
   let aligned_ptr{.restrict.} = block: # We cannot directly apply restrict to the default "result"
     let remainder = address and (LASER_MEM_ALIGN - 1) # modulo LASER_MEM_ALIGN (power of 2)
     if remainder == 0:
       assume_aligned cast[ptr UncheckedArray[T]](address)
     else:
       let offset = LASER_MEM_ALIGN - remainder
-      assume_aligned cast[ptr UncheckedArray[T]](address +% offset)
+      assume_aligned cast[ptr UncheckedArray[T]](address + offset)
   return aligned_ptr

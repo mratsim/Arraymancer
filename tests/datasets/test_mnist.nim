2aef9ba22d8c82e53eff9fc63702b44865418eb2
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2023-10-09 12:56:14 +0200

    Fix fashion mnist loading digits mnist (#596)

--- tests/datasets/test_mnist.nim
+++ tests/datasets/test_mnist.nim
@@ -14,12 +14,15 @@ proc main() =
         mnist.train_labels.shape == [60000]
         mnist.test_labels.shape == [10000]
     test "Load Fashion MNIST":
-      let mnist = load_mnist(cache = true, fashion_mnist = true)
+      let
+        mnist = load_mnist(cache = true)
+        fashion_mnist = load_mnist(cache = true, fashion_mnist = true)
       check:
-        mnist.train_images.shape == [60000, 28, 28]
-        mnist.test_images.shape == [10000, 28, 28]
-        mnist.train_labels.shape == [60000]
-        mnist.test_labels.shape == [10000]
+        fashion_mnist.train_images.shape == [60000, 28, 28]
+        fashion_mnist.test_images.shape == [10000, 28, 28]
+        fashion_mnist.train_labels.shape == [60000]
+        fashion_mnist.test_labels.shape == [10000]
+        fashion_mnist.train_images != mnist.train_images
 
 main()
 GC_fullCollect()

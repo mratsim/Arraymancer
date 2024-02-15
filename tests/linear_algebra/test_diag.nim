4560df33461c740c388e01794b6f39496e28dc2d
Angel Ezquerra <angel.ezquerra@gmail.com>
2024-02-15 13:26:31 +0100

    Add tests for the tri(), tril() and triu() procedures.
    Tests have been added to test_diag.nim which will be renamed to test_special_matrices.nim.

--- tests/linear_algebra/test_diag.nim
+++ tests/linear_algebra/test_diag.nim
@@ -22,6 +22,7 @@ proc main() =
                             [0.0, 0.0, 1.0, 0.0]].toTensor
         check:
           eye == expected_eye
+
     test "Diagonal Matrix creation (diag)":
       block:
         # Issue: should implement anti?
@@ -41,7 +42,7 @@ proc main() =
           square_diag == expected_square_diag
           upper1_diag == expected_upper1_diag
           lower1_diag == expected_lower1_diag
-        
+
     test "Get diagonal":
       block:
         let a = arange(4*5).reshape(4, 5)
@@ -68,6 +69,7 @@ proc main() =
           a.diagonal(anti=true) == expected_center_antidiagonal
           a.diagonal(-1, anti=true) == expected_lower1_antidiagonal
           a.diagonal(-2, anti=true) == expected_lower2_antidiagonal
+
     test "Set diagonal":
       block:
         var a = zeros[int](3, 4).with_diagonal([10, 20, 30].toTensor, 1)
@@ -92,6 +94,32 @@ proc main() =
         check:
           a == expected_a
 
+    test "Triangular Matrices":
+      block:
+        let expected_t1 = [[1.0, 0.0, 0.0],
+                           [1.0, 1.0, 0.0],
+                           [1.0, 1.0, 1.0]].toTensor
+        let expected_t2 = [[1.0, 1.0, 1.0],
+                           [0.0, 1.0, 1.0],
+                           [0.0, 0.0, 1.0]].toTensor
+        let expected_t3 = [[0.0, 1.0, 1.0],
+                           [0.0, 0.0, 1.0],
+                           [0.0, 0.0, 0.0]].toTensor
+        let t1 = tri[float](3, 3)
+        let t2 = tri[float](3, 3, upper = true)
+        let t3 = tri[float](3, 3, k=1, upper = true)
+
+        let t1b = tri[float](t1.shape)
+        let t2b = tri[float](t1.shape, upper = true)
+        let t3b = tri[float](t1.shape, k = 1, upper = true)
+
+        check:
+          t1 == expected_t1
+          t2 == expected_t2
+          t3 == expected_t3
+          t1b == expected_t1
+          t2b == expected_t2
+          t3b == expected_t3
 
 main()
 GC_fullCollect()

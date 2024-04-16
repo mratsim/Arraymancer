af64112c1897db709f762ec3aa47be89d7c0ca95
Angel Ezquerra <angel.ezquerra@gmail.com>
2024-04-16 20:41:06 +0200

    Add `unique` functions to algorithms.nim
    These functions are similar to but not as fully featured as `numpy.unique`.
    They are missing a way to count the number or returning the indexes of the unique elements.
    However, they make it possibel to (optionally) sort the output, or to use a more efficient algorithm if the input is already sorted.

--- tests/tensor/test_algorithms.nim
+++ tests/tensor/test_algorithms.nim
@@ -50,3 +50,24 @@ suite "[Core] Testing algorithm functions":
       let idxSorted = t.argsort(order = SortOrder.Descending)
       check idxSorted == exp
       check t[idxSorted] == @[7, 4, 3, 2, 1].toTensor()
+
+  test "Unique":
+    block:
+      let
+        dup = [1, 3, 2, 4, 1, 8, 2, 1, 4].toTensor
+        unique_unsorted = dup.unique
+        unique_presorted_ascending = sorted(dup.unique).unique(isSorted = true)
+        unique_presorted_descending = sorted(dup.unique, order = SortOrder.Descending).unique(isSorted = true)
+        unique_sorted_ascending = dup.unique(order = SortOrder.Ascending)
+        unique_sorted_descending = dup.unique(order = SortOrder.Descending)
+        dup_not_C_continuous = dup[_ | 2]
+        unique_not_c_continuous = dup_not_C_continuous.unique
+        unique_sorted_not_c_continuous = dup_not_C_continuous.unique(order = SortOrder.Descending)
+
+      check unique_unsorted == [1, 3, 2, 4, 8].toTensor
+      check unique_presorted_ascending == [1, 2, 3, 4, 8].toTensor
+      check unique_presorted_descending == [8, 4, 3, 2, 1].toTensor
+      check unique_sorted_ascending == [1, 2, 3, 4, 8].toTensor
+      check unique_sorted_descending == [8, 4, 3, 2, 1].toTensor
+      check unique_not_c_continuous == [1, 2, 4].toTensor
+      check unique_sorted_not_c_continuous == [4, 2, 1].toTensor

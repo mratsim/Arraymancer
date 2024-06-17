545491dbec03c9e440a077ed7ebf42b6fdace3f6
Angel Ezquerra <angel.ezquerra@gmail.com>
2024-06-17 20:33:45 +0200

    Add "format string" support for tensors
    This comes in 2 forms:
    1. Add a version of the pretty function that takes a format string as its input instead of a precision value. Also add a new "showHeader" argument to the original pretty function.
        - This new version of pretty lets you specify the format string that must be used to display each element. It also adds some new tensor-specific format string "tokens" that are used to control how tensors are displayed (beyond the format of each element). See below for more details.
    2. Add a formatValue procedure that takes a tensor as its first input. This makes it possible to control how tensors are displayed in format strings, in the exact same way as if you were using the new pretty(specifier) procedure.
    
    This new formatValue procedure takes all of the standard format specifiers, such as "f", "g", "+", etc. (and including, in nim 2.2 and above, the 'j' specifier for complex tensors) which are used to configure of each tensor element is displayed.
    In addition to those standard format specifiers you can use a few, tensor specific modifiers which can be combined to achieve different results:
    
    - "[:]": Display the tensor as if it were a nim "array".
             This makes it easy to use the representation of a tensor in your own code. No header is shown.
    - "<>[:]": Same as "[:]" but displays a header describing the tensor type and shape.
    - "[]": Same as "[:]" but displays the tensor in a single line. No header is shown.
    - "<>[:]": Same as "[]" but displays a header describing the tensor type and shape.
    - "||": "Pretty-print" the tensor _without_ a header.
    - "<>||": Same as "||" but displays a header describing the tensor type and shape. This is the default display mode.
    - "<>": When used on its own (i.e. not combined with "[:]", "[]" and "||" as shown above) it is equivalent to "<>[]" (i.e. single-line, nim-like representation with a header). Cannot wrap the element specifier.
    
    Notes:
    - The default format (i.e. when none of these tensor-specific tokens is used) is pretty printing with a header (i.e. "<>||").
    - These tensor specific modifiers can placed at the start or at the end of the format specifier (e.g. "<>[:]06.2f", or "06.2f<>[:]"), or they
    can "wrap" the element specifier (e.g. "<>[:06.2f]").
    - When wrapping you cannot use the "compact forms" of these specifiers (i.e. "<:>" and "<|>").
    - When using the wrapping form of "[:]", start with "[:" and end with either "]" or ":]" (i.e. both "[:06.2f]" and "[:06.2f:]" are valid).
    - This version of this function does not have a `showHeader` argument because to disable the header you can simply select the right format
    specifier.
    
    Fixes after PR comments
    
    More fixes

--- src/arraymancer/tensor/private/p_display.nim
+++ src/arraymancer/tensor/private/p_display.nim
@@ -14,15 +14,140 @@
 
 import  ../../private/functional, ../higher_order_applymap,
         ../shapeshifting, ../data_structure,
-        ../accessors,
-        sequtils, strutils
+        ../accessors
+import std / [sequtils, strutils, strformat, typetraits]
+
+type tensorDispMode = enum table, multi_line_array, single_line_array
+
+func isWrappedBy(s: string, prefix: string, suffix: string): bool =
+  ## Check if a string starts or ends with a given prefix and suffix
+  return s.startsWith(prefix) and s.endsWith(suffix)
+
+func startsOrEndsWith(s: string, token: string): bool =
+  ## Check if a string starts or ends with a given prefix or suffix
+  return s.startsWith(token) or s.endsWith(token)
+
+func parseTensorFormatSpecifier(specifier: static string): (tensorDispMode, bool, string) =
+  ## Parse the tensor format specifier
+  ##
+  ## This custom made parser takes a "tensor format specifier" and returs the
+  ## display mode, a boolean indicating whether a header must be shown, and the
+  ## element format specifier
+  when specifier == "" or ("[" notin specifier and "<" notin specifier and "|" notin specifier and ":" notin specifier):
+    # Fast path for the most common, default case, which is getting an empty or
+    # an element format specifier without any special tensor markers
+    return (table, true, specifier)
+  # Handle the "shortcut" markers, which can appear either at the start or the
+  # end of the specifier, first
+  elif "[:]" in specifier or "<:>" in specifier or "[]" in specifier or "||" in specifier or "<|>" in specifier:
+    const element_specifier = specifier.multiReplace(
+      ("[:]", ""), ("<:>", ""), ("[]", ""), ("||", ""), ("<|>", ""))
+    const has_header = "<>" in specifier or "<:>" in specifier or "<|>" in specifier
+    when specifier.startsOrEndsWith("[:]") or specifier.startsOrEndsWith("<>[:]") or specifier.startsOrEndsWith("<:>"):
+      return (multi_line_array, has_header, element_specifier)
+    elif specifier.startsOrEndsWith("[]") or specifier.startsOrEndsWith("<>[]"):
+      return (single_line_array, has_header, element_specifier)
+    elif specifier.startsOrEndsWith("||") or specifier.startsOrEndsWith("<>||") or specifier.startsOrEndsWith("<|>"):
+      return (table, has_header, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "[:], [], ||, <:>, <|>, <>[:], <>[] and <>|| must be located at the " &
+        "start or the end of the format specifier."
+      .}
+  # Then handle the "wrapper" markers, which have separate opening and closing
+  # tokens which must appear that the start and the end of the specifier
+  # Start with the "<>" markers (i.e. those that request displaying a header)
+  elif "<>[:" in specifier:
+    # Multi-line with header
+    when specifier.isWrappedBy("<>[:", ":]"):
+      const element_specifier = specifier[4 ..< ^2]
+      return (multi_line_array, true, element_specifier)
+    elif specifier.isWrappedBy("<>[:", "]"):
+      # Support skipping the : on the closing bracket
+      const element_specifier = specifier[4 ..< ^1]
+      return (multi_line_array, true, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "<>[: and ] must wrap the element format specifier."
+      .}
+  elif "<>[" in specifier:
+    when specifier.isWrappedBy("<>[", "]"):
+      const element_specifier = specifier[3 ..< ^1]
+      return (single_line_array, true, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "<>[ and ] wrap the element format specifier."
+      .}
+  elif "<>|" in specifier:
+    when specifier.isWrappedBy("<>|", "|"):
+      const element_specifier = specifier[3 ..< ^1]
+      return (table, true, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "<>| and | must wrap the element format specifier."
+      .}
+  # Then handle the "<>-less" wrapper markers
+  elif "[:" in specifier:
+    when specifier.isWrappedBy("[:", ":]"):
+      const element_specifier = specifier[2 ..< ^2]
+      return (multi_line_array, false, element_specifier)
+    elif specifier.isWrappedBy("[:", "]"):
+      # Support skipping the : on the closing bracket
+      const element_specifier = specifier[2 ..< ^1]
+      return (multi_line_array, false, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "[: and ] must wrap the element format specifier."
+      .}
+  elif "[" in specifier:
+    when specifier.isWrappedBy("[", "]"):
+      const element_specifier = specifier[1 ..< ^1]
+      return (single_line_array, false, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "[ and ] must wrap the element format specifier."
+      .}
+  elif "|" in specifier:
+    when specifier.isWrappedBy("|", "|"):
+      const element_specifier = specifier[1 ..< ^1]
+      return (table, false, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "| and | must wrap the element format specifier."
+      .}
+  # At this point "<>" must appear on its own (meaning the same as "<>[]")
+  elif "<>" in specifier:
+    when specifier.startsOrEndsWith("<>"):
+      const element_specifier = specifier.replace("<>")
+      return (single_line_array, true, element_specifier)
+    else:
+      {. error:
+        "Invalid tensor format specifier (" & specifier & "): " &
+        "<> must be located at the start or the end of the format specifier."
+      .}
+  else:
+    # This should never happen, but if it did, use the default display format
+    return (table, true, specifier)
+
+func removeTensorFormatSpecifiers(specifier: static string): string =
+  ## Remove the special "tensor specifier tokens"
+  const (dispMode {.used.}, showHeader {.used.}, elementSpecifier)
+    = parseTensorFormatSpecifier(specifier)
+  return elementSpecifier
 
 func bounds_display(t: Tensor,
                     idx_data: tuple[val: string, idx: int],
                     alignBy, alignSpacing: int
           ): string =
   ## Internal routine, compare an index with the strides of a Tensor
-  ## to check beginning and end of lines
+  ## to check beginning and end of lines
   ## Add the delimiter "|" and line breaks at beginning and end of lines
   ##
   ## `alignBy` is the total fill for each "column" in a 2D print. `alignSpacing`
@@ -78,17 +203,24 @@ func bounds_display(t: Tensor,
 # let b = toSeq(1..72).toTensor.reshape(2,3,3,4)
 # echo b
 
+func dispElement*[T](value: T, precision = -1, specifier: static string = ""): string =
+  ## Display a single element with the selected precision _or_ specifier format
+  when specifier.len == 0:
+    when T is SomeFloat:
+      result = formatBiggestFloat(value, precision = precision)
+    else:
+      result = $value
+  else:
+    formatValue(result, value, specifier = specifier)
+
 func disp2d*[T](t: Tensor[T], alignBy = 6, alignSpacing = 3,
-                precision = -1): string =
-  ## Display a 2D-tensor
+                precision = -1, specifier: static string = ""): string =
+  ## Display a 2D-tensor (only used for "table", i.e. non array, printing)
 
   # Add a position index to each value in the Tensor.
   var indexed_data: seq[(string,int)] = @[]
   for i, value in t.enumerate:
-    when T is SomeFloat:
-      let val = formatBiggestFloat(value, precision = precision)
-    else:
-      let val = $value
+    let val = dispElement(value, precision = precision, specifier = specifier)
     indexed_data.add((val, i+1))  # TODO Note: the $conversion is unstable if the whole test suite is done.
                                   # it fails at the test_load_openmp.
                                   # if done alone there is no issue
@@ -152,17 +284,15 @@ func genLeftIdx(axIdx: string, s: string): string =
     if i < tmp.high - 1:
       result.add "\n"
 
-proc determineLargestElement[T](t: Tensor[T], precision: int): int =
+proc determineLargestElement[T](t: Tensor[T], precision: int, specifier: static string = ""): int =
   ## Determines the length of the "largest" element in the tensor after
   ## string conversion. This is to align our output table nicely.
-  when T is SomeFloat:
-    result = t.map_inline((x.formatBiggestFloat(precision = precision)).len).max
-  else:
-    result = t.map_inline(($x).len).max
+  result = t.map_inline(x.dispElement(precision = precision, specifier = specifier).len).max
 
-proc prettyImpl*[T](t: Tensor[T],
-                    inputRank = 0, alignBy = 0, alignSpacing = 4,
-                    precision = -1): string =
+proc dispTensorAsTable*[T](t: Tensor[T],
+                           inputRank = 0, alignBy = 0, alignSpacing = 4,
+                           precision = -1,
+                           specifier: static string = ""): string =
   ## Pretty printing implementation that aligns N dimensional tensors as a
   ## table. Odd dimensions are stacked horizontally and even dimensions
   ## vertically.
@@ -174,11 +304,12 @@ proc prettyImpl*[T](t: Tensor[T],
   ## and all others right aligned).
   ##
   ## `precision` sets the floating point precision.
+  const elementSpecifier = removeTensorFormatSpecifiers(specifier)
   var alignBy = alignBy
   var inputRank = inputRank
   if inputRank == 0:
     inputRank = t.rank
-    let largestElement = t.determineLargestElement(precision)
+    let largestElement = t.determineLargestElement(precision, elementSpecifier)
     alignBy = max(6, largestElement + alignSpacing)
   # for tensors of rank larger 2, walk axis 0 and stack vertically (even dim)
   # or stack horizontally (odd dim)
@@ -189,8 +320,11 @@ proc prettyImpl*[T](t: Tensor[T],
     for ax in axis(t, 0):
       if oddRank:
         # 1. get next "column"
-        var toZip = prettyImpl(ax.squeeze, inputRank, alignBy = alignBy,
-                               precision = precision)
+        var toZip = dispTensorAsTable(ax.squeeze,
+                                      inputRank,
+                                      alignBy = alignBy,
+                                      precision = precision,
+                                      specifier = elementSpecifier)
         # 2. center current "column" index to width of `toZip`, put on top
         toZip = center($axIdx, toZip.splitLines[0].len) & "\n" & toZip
         # 3. generate separator of "columns" and zip together
@@ -198,8 +332,11 @@ proc prettyImpl*[T](t: Tensor[T],
         res = res.zipStrings(toZip, sep = sep, allowEmpty = false)
       else:
         # 1. get next "row"
-        var toStack = prettyImpl(ax.squeeze, inputRank, alignBy = alignBy,
-                                 precision = precision)
+        var toStack = dispTensorAsTable(ax.squeeze,
+                                        inputRank,
+                                        alignBy = alignBy,
+                                        precision = precision,
+                                        specifier = elementSpecifier)
         # 2. center current "row" index to height of `toStack`
         let leftIdx = genLeftIdx($axIdx, toStack)
         # 3. zip index and "row"
@@ -215,4 +352,134 @@ proc prettyImpl*[T](t: Tensor[T],
   else:
     result = t.disp2d(alignBy = alignBy,
                       alignSpacing = alignSpacing,
-                      precision = precision).strip
+                      precision = precision,
+                      specifier = elementSpecifier).strip
+
+proc disp1dAsArray[T](t: Tensor[T],
+                    sep = ", ",
+                    precision = -1, specifier: static string = ""): string =
+  ## Display a 1D-tensor (only used for "array-style", i.e. non-table, printing)
+  if t.len == 0:
+    return "[]"
+  result = "["
+  for value in t:
+    result &= dispElement(value, precision = precision, specifier = specifier)
+    result &= sep
+  # Remove the separator from the last element
+  result = result[0..^(1+sep.len)] & "]"
+  when T is Complex and "j" in specifier:
+    result = result.replace("(").replace(")")
+
+proc compactTensorDescription[T](t: Tensor[T]): string =
+  ## Describe the tensor in terms of its shape and type (in a "compact" way)
+  ## Only used for array-style printing
+  # Most if not all tensors element types are part of the system or complex
+  # modules so we can remove them from the type without much information loss
+  let compactType = t.type.name().replace("system.", "").replace("complex.", "")
+  let compactShape = ($t.shape)[1 ..< ^1].replace(", ", ",")
+  result = compactType & "<" & compactShape & ">"
+
+proc squeezeTopDimension[T](t: Tensor[T]): Tensor[T] =
+  ## Remove the top most dimension if its size is 1
+  if t.shape.len == 0 or t.shape[0] > 1:
+    return t
+  var new_shape = t.shape
+  new_shape.delete(0)
+  result = t.reshape(new_shape)
+
+proc dispTensorAsSingleLineArrayImp[T](t: Tensor[T],
+                    precision = -1,
+                    specifier: static string = "",
+                    indentSpacing = 0,
+                    sep = ", ", rowSep = ""
+                    ): string =
+  ## Implementation of the "array-style" tensor printing
+  result = "["
+  if t.rank <= 1:
+    result = disp1dAsArray(t, sep = sep, precision = precision, specifier = specifier)
+  else:
+    var n = 0
+    for ax in axis(t, 0):
+      var axRepr = dispTensorAsSingleLineArrayImp(ax.squeezeTopDimension(),
+        precision = precision,
+        specifier = specifier,
+        indentSpacing = indentSpacing,
+        sep = sep, rowSep = rowSep)
+      result &= axRepr
+      n += 1
+      if n < t.shape[0]:
+        result &= sep & rowSep
+    result &= "]"
+
+proc dispTensorAsSingleLineArray*[T](t: Tensor[T],
+                    precision = -1,
+                    indentSpacing = 0,
+                    specifier: static string = "",
+                    sep = ", ", rowSep = "",
+                    showHeader = true
+                    ): string =
+  ## Display a tensor as a single line "array"
+  # Remove the non-standard specifier flags
+  const elementSpecifier = removeTensorFormatSpecifiers(specifier)
+  if showHeader:
+    result = t.compactTensorDescription & ":" & rowSep
+  result &= dispTensorAsSingleLineArrayImp(t, precision, specifier = elementSpecifier, rowSep = rowSep)
+  if t.storage.isNil:
+    # Return a useful message for uninit'd tensors instead of crashing
+    # Note that this should only happen when displaying tensors created
+    # by just declaring their type (e.g. `var t: Tensor[int]`), given that
+    # even tensors created by calling `newTensorUninit` have their storage
+    # initialized (with garbage)
+    result &= " (uninitialized)"
+
+proc indentTensorReprRows(s: string, indent: int): string =
+  ## Indent the lines of a multi-line "array-style" tensor representation
+  ## so that the right-most opening braces align vertically
+  if indent <= 0:
+    return s
+  for line in s.splitLines():
+    var numBrackets = 0
+    for c in line:
+      if c != '[':
+        break
+      numBrackets += 1
+    result &= line.indent(indent - numBrackets) & "\n"
+
+proc dispTensorAsArray*[T](t: Tensor[T],
+                    precision = -1,
+                    specifier: static string = "",
+                    showHeader = true): string =
+  ## Display a tensor as a multi-line "array"
+  result = t.dispTensorAsSingleLineArray(
+    precision = precision, specifier = specifier, rowSep="\n", showHeader = false)
+  result = indentTensorReprRows(result, t.rank).strip(leading=false)
+  if showHeader:
+    result = t.compactTensorDescription() & ":\n" & result
+
+proc prettyImpl*[T](
+    t: Tensor[T], precision: int, specifier: static string): string =
+  ## Non public implementation of the pretty function
+  ## Three modes are supported: table, multi-line array and single-line array
+  const (dispMode, showHeader, elementSpecifier) = parseTensorFormatSpecifier(specifier)
+  if dispMode == single_line_array:
+    return t.dispTensorAsSingleLineArray(
+      precision = precision, specifier = elementSpecifier, showHeader = showHeader)
+  elif dispMode == multi_line_array:
+    return t.dispTensorAsArray(
+      precision = precision, specifier = elementSpecifier, showHeader = showHeader)
+  # Represent the tensor as a "pretty" table
+  var desc = t.type.name & " of shape \"" & $t.shape & "\" on backend \"" & "Cpu" & "\""
+  if t.storage.isNil: # return useful message for uninit'd tensors instead of crashing
+    return "Uninitialized " & $desc
+  if showHeader:
+    desc &= "\n"
+  else:
+    desc = ""
+  if t.size() == 0:
+    return desc & "    [] (empty)"
+  elif t.rank == 1: # for rank 1 we want an indentation, because we have no `|`
+    return desc & "    " & t.dispTensorAsTable(
+      precision = precision, specifier = elementSpecifier)
+  else:
+    return desc & t.dispTensorAsTable(
+      precision = precision, specifier = elementSpecifier)

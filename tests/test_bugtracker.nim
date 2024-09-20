mratsim/Arraymancer

commit 35adfc10a21dd54e7695bc653a4c8c0887a3f48e

Non-contiguous tensor iteration optimization (#659)
* optimized stridedIteration for non-contiguous tensors by explicitly looping over last two axes

* fixed an introduced stridedIteration bug for iter_offset != 0 or iter_size != tensor size

* made reshape use map_inline instead of apply2_inline

* fixed a bug in reshape modification (was returning unintialized tensor when copying)

* refactored the loop code, added asserts for better clarity

* added a comment for the stridedIterationLoop

src/arraymancer/tensor/private/p_accessors.nim
@@ -57,6 +57,53 @@ import  ../backend/[global_config, memory_optimization_hints],
 #         coord[k] = 0
 #         iter_pos -= backstrides[k]
 
+type TensorForm = object
+  shape: Metadata
+  strides: Metadata
+
+proc rank(t: TensorForm): range[0 .. LASER_MAXRANK] {.inline.} =
+  t.shape.len
+
+func size(t: TensorForm): int {.inline.} =
+  result = 1
+  for i in 0..<t.rank:
+    result *= t.shape[i]
+
+func reduceRank(t: TensorForm): TensorForm =
+  result = t
+
+  var i = 0
+  result.shape[0] = t.shape[0]
+  result.strides[0] = t.strides[0]
+  for j in 1..<t.rank:
+    #spurious axis
+    if t.shape[j] == 1:
+      continue
+
+    #current axis is spurious
+    if result.shape[i] == 1:
+      result.shape[i] = t.shape[j]
+      result.strides[i] = t.strides[j]
+      continue
+
+    #axes can be coalesced
+    if result.strides[i] == t.shape[j]*t.strides[j]:
+      result.shape[i] = result.shape[i]*t.shape[j]
+      result.strides[i] = t.strides[j]
+      continue
+
+    i += 1
+    result.shape[i] = t.shape[j]
+    result.strides[i] = t.strides[j]
+  result.shape.len = i + 1
+  result.strides.len = i + 1
+
+func floor(x: int, divisor: int): int {.inline.} =
+  return divisor*(x div divisor)
+
+func ceil(x: int, divisor: int): int {.inline.} =
+  return divisor*(((x - 1) div divisor) + 1)
+
 proc getIndex*[T](t: Tensor[T], idx: varargs[int]): int {.noSideEffect,inline.} =
   ## Convert [i, j, k, l ...] to the proper index.
   when compileOption("boundChecks"):
@@ -166,25 +213,145 @@ template stridedIterationYield*(strider: IterKind, data, i, iter_pos: typed) =
   elif strider == IterKind.Iter_Values: yield (i, data[iter_pos])
   elif strider == IterKind.Offset_Values: yield (iter_pos, data[iter_pos]) ## TODO: remove workaround for C++ backend
 
+template stridedIterationLoop*(strider: IterKind, data, t, iter_offset, iter_size, prev_d, last_d: typed) =
+  ## We break up the tensor in 5 parts and iterate over each using for loops.
+  ## We do this because the loop ranges and nestedness are different for each part.
+  ## The part boundaries are calculated and stored in the `bp1`, `bp2`, `bp3`
+  ## and `bp4` variables. The `(iter_offset, bp1)` segment is a rank-1 tensor
+  ## of size `<last_d`. The `(bp1, bp2)` segment is a rank-2 tensor with first
+  ## axis smaller than `prev_d`. The `(bp2, bp3)` segment is the main body, an
+  ## rank-n tensor with last axes sizes `prev_d` and `last_d`. The `(bp3, bp4)`
+  ## segment is a rank-2 tensor, and the `(bp4, iter_offset + iter_size)` segment
+  ## is a rank-1 tensor.
+  assert t.rank > 1
+
+  let prev_s = t.strides[^2]
+  let last_s = t.strides[^1]
+  let rank = t.rank
+  let size = t.size
+
+  assert iter_offset >= 0
+  assert iter_size <= size - iter_offset
+  assert prev_d > 0 and last_d > 0
+  assert size mod prev_d*last_d == 0
+
+  initStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
+
+  let bp1 =
+    if iter_offset == 0:
+      0
+    else:
+      min(iter_offset + iter_size, ceil(iter_offset, last_d))
+  let bp2 =
+    if iter_offset == 0:
+      0
+    else:
+      max(bp1, min(floor(iter_offset + iter_size, prev_d*last_d), ceil(iter_offset, prev_d*last_d)))
+  let bp3 =
+    if iter_size == size:
+      size
+    else:
+      max(bp2, floor(iter_offset + iter_size, prev_d*last_d))
+  let bp4 =
+    if iter_size == size:
+      size
+    else:
+      max(bp3, floor(iter_offset + iter_size, last_d))
+
+  assert iter_offset <= bp1 and bp1 <= bp2 and bp2 <= bp3 and bp3 <= bp4 and bp4 <= iter_offset + iter_size
+  assert bp1 - iter_offset < last_d and (bp1 mod last_d == 0 or bp1 == iter_offset + iter_size)
+  assert bp2 == bp1 or (bp2 mod prev_d*last_d == 0 and bp2 - bp1 < prev_d*last_d)
+  assert bp3 == bp2 or bp3 mod prev_d*last_d == 0
+  assert bp4 == bp3 or (bp4 mod last_d == 0 and bp4 - bp3 < prev_d*last_d)
+  assert iter_offset + iter_size - bp4 < last_d
+
+  var i = iter_offset
+
+  if bp1 > iter_offset:
+    coord[rank - 1] += bp1 - i - 1
+    while i < bp1:
+      stridedIterationYield(strider, data, i, iter_pos)
+      iter_pos += last_s
+      i += 1
+    iter_pos -= last_s
+    advanceStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
+
+  if bp2 > bp1:
+    coord[rank - 2] += ((bp2 - i) div last_d) - 1
+    coord[rank - 1] = last_d - 1
+    while i < bp2:
+      for _ in 0..<last_d:
+        stridedIterationYield(strider, data, i, iter_pos)
+        iter_pos += last_s
+        i += 1
+      iter_pos += prev_s - last_s*last_d
+    iter_pos += last_s*(last_d - 1) - prev_s
+    advanceStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
+
+  while i < bp3:
+    for _ in 0..<prev_d:
+      for _ in 0..<last_d:
+        stridedIterationYield(strider, data, i, iter_pos)
+        iter_pos += last_s
+        i += 1
+      iter_pos += prev_s - last_s*last_d
+    iter_pos -= prev_s*prev_d
+
+    for k in countdown(rank - 3, 0):
+      if coord[k] < t.shape[k] - 1:
+        coord[k] += 1
+        iter_pos += t.strides[k]
+        break
+      else:
+        coord[k] = 0
+        iter_pos -= backstrides[k]
+
+  if bp4 > bp3:
+    coord[rank - 2] += ((bp4 - i) div last_d) - 1
+    coord[rank - 1] = last_d - 1
+    while i < bp4:
+      for _ in 0..<last_d:
+        stridedIterationYield(strider, data, i, iter_pos)
+        iter_pos += last_s
+        i += 1
+      iter_pos += prev_s - last_s*last_d
+    iter_pos += last_s*(last_d - 1) - prev_s
+    advanceStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
+
+  while i < iter_offset + iter_size:
+    stridedIterationYield(strider, data, i, iter_pos)
+    iter_pos += last_s
+    i += 1
+
 template stridedIteration*(strider: IterKind, t, iter_offset, iter_size: typed): untyped =
   ## Iterate over a Tensor, displaying data as in C order, whatever the strides.
 
   # Get tensor data address with offset builtin
   # only reading here, pointer access is safe even for ref types
+
   when getSubType(type(t)) is KnownSupportsCopyMem:
     let data = t.unsafe_raw_offset()
   else:
     template data: untyped {.gensym.} = t
 
-  # Optimize for loops in contiguous cases
-  if t.is_C_contiguous:
+  let tf = reduceRank(TensorForm(shape: t.shape, strides: t.strides))
+
+  assert tf.rank >= 1
+  if tf.rank == 1:
+    let s = tf.strides[^1]
     for i in iter_offset..<(iter_offset+iter_size):
-      stridedIterationYield(strider, data, i, i)
+      stridedIterationYield(strider, data, i, i*s)
   else:
-    initStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
-    for i in iter_offset..<(iter_offset+iter_size):
-      stridedIterationYield(strider, data, i, iter_pos)
-      advanceStridedIteration(coord, backstrides, iter_pos, t, iter_offset, iter_size)
+    let prev_d = tf.shape[^2]
+    let last_d = tf.shape[^1]
+    if prev_d == 2 and last_d == 2:
+      stridedIterationLoop(strider, data, tf, iter_offset, iter_size, 2, 2)
+    elif last_d == 2:
+      stridedIterationLoop(strider, data, tf, iter_offset, iter_size, prev_d, 2)
+    elif last_d == 3:
+      stridedIterationLoop(strider, data, tf, iter_offset, iter_size, prev_d, 3)
+    else:
+      stridedIterationLoop(strider, data, tf, iter_offset, iter_size, prev_d, last_d)
 
 template stridedCoordsIteration*(t, iter_offset, iter_size: typed): untyped =
   ## Iterate over a Tensor, displaying data as in C order, whatever the strides. (coords)

src/arraymancer/tensor/private/p_shapeshifting.nim
@@ -30,14 +30,14 @@ proc contiguousImpl*[T](t: Tensor[T], layout: OrderType, result: var Tensor[T]) 
     apply2_inline(result, t):
       y
 
-proc reshape_with_copy*[T](t: Tensor[T], new_shape: varargs[int]|Metadata|seq[int], result: var Tensor[T]) =
-  result = newTensorUninit[T](new_shape)
-  result.apply2_inline(t,y)
-
 proc reshape_no_copy*(t: AnyTensor, new_shape: varargs[int]|Metadata|seq[int], result: var AnyTensor, layout: OrderType) {.noSideEffect.}=
   result.shape.copyFrom(new_shape)
   shape_to_strides(result.shape, layout, result.strides)
   result.offset = t.offset
+
+proc reshape_with_copy*[T](t: Tensor[T], new_shape: varargs[int]|Metadata|seq[int], result: var Tensor[T]) =
+  contiguousImpl(t, rowMajor, result)
+  reshape_no_copy(t, new_shape, result, rowMajor)
 
 proc infer_shape*(t: Tensor, new_shape: varargs[int]): seq[int] {.noinit.} =
   ## Replace the single -1 value on `new_shape` with the value that

commit 5f8ef9d378c666269c6b5a51fa5641aa9288f354

Explicitly allow `openArray` in Tensor `[]`, `[]=`, fix the CI (#666)
* explicitly allow `openArray` in `[]`, `[]=` for tensors

This was simply an oversight obviously

* fix CI by compiling tests with `-d:ssl`

* need a space, duh

* use AWS mirror from PyTorch for MNIST download

* fix regression caused by PR #659

By not resetting the offset here, operating on a Tensor view without
cloning could cause undefined behavior, because we would be accessing
elements outside the tensor buffer.

* add test case for regression of #659

arraymancer.nimble
@@ -143,9 +143,9 @@ proc test(name, switches = "", split = false, lang = "c") =
   if not dirExists "build":
     mkDir "build"
   if not split:
-    exec "nim " & lang & " -o:build/" & name & switches & " -r tests/" & name & ".nim"
+    exec "nim " & lang & " -d:ssl -o:build/" & name & switches & " -r tests/" & name & ".nim"
   else:
-    exec "nim " & lang & " -o:build/" & name & switches & " -r tests/_split_tests/" & name & ".nim"
+    exec "nim " & lang & " -d:ssl -o:build/" & name & switches & " -r tests/_split_tests/" & name & ".nim"
 
   # run tests that require old RNG for backward compat. reasos
   let rngTests = ["spatial/test_kdtree",

src/arraymancer/datasets/mnist.nim
@@ -71,7 +71,11 @@ const MNISTFilenames = [
 ]
 
 const
-  DefaultMnistUrl = "http://yann.lecun.com/exdb/mnist/"
+  ## NOTE: As of some time before 2024/09/20 the MNIST dataset cannot be downloaded from
+  ## Yann's website anylonger, due to 403 error.
+  ## The AWS mirror here comes from PyTorch Vision:
+  ## https://github.com/pytorch/vision/blob/6d7851bd5e2bedc294e40e90532f0e375fcfee04/torchvision/datasets/mnist.py#L39C10-L39C56
+  DefaultMnistUrl = "https://ossci-datasets.s3.amazonaws.com/mnist/" # "http://yann.lecun.com/exdb/mnist/"
   FashionMnistUrl = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
 
 proc read_mnist_images(stream: Stream): Tensor[uint8] {.noinit.} =

src/arraymancer/tensor/private/p_accessors.nim
@@ -496,7 +496,7 @@ proc checkValidSliceType*(n: NimNode) =
   ## the `[]` and `[]=` macros. It will raise a CT error in case it is not.
   ##
   ## TODO: Do we / should we allow other integer types than `tyInt` / `int`?
-  const validTypes = {ntyInt, ntyObject, ntyArray, ntySequence, ntyGenericInst}
+  const validTypes = {ntyInt, ntyObject, ntyArray, ntySequence, ntyOpenArray, ntyGenericInst}
   # `ntyObject` requires to be `Span`, ...
   template raiseError(arg: untyped): untyped =
     let typ = arg.getTypeInst
@@ -508,7 +508,7 @@ proc checkValidSliceType*(n: NimNode) =
     of validTypes:
       if arg.typeKind in {ntyObject, ntyGenericInst} and not validObjectType(arg):
         raiseError(arg)
-      elif arg.typeKind in {ntyArray, ntySequence}:
+      elif arg.typeKind in {ntyArray, ntySequence, ntyOpenArray}:
         # Need to check inner type!
         checkValidSliceType(arg.getTypeInst()[^1])
         break

src/arraymancer/tensor/private/p_shapeshifting.nim
@@ -38,6 +38,7 @@ proc reshape_no_copy*(t: AnyTensor, new_shape: varargs[int]|Metadata|seq[int], r
 proc reshape_with_copy*[T](t: Tensor[T], new_shape: varargs[int]|Metadata|seq[int], result: var Tensor[T]) =
   contiguousImpl(t, rowMajor, result)
   reshape_no_copy(t, new_shape, result, rowMajor)
+  result.offset = 0 # Offset needs to be reset! Copy is a new tensor of `new_shape`
 
 proc infer_shape*(t: Tensor, new_shape: varargs[int]): seq[int] {.noinit.} =
   ## Replace the single -1 value on `new_shape` with the value that

tests/test_bugtracker.nim
@@ -64,5 +64,51 @@ proc main() =
           x.permute(1, 0) == expected
           x.permute(1, 0).reshape(2, 2) == expected
 
+    test "Test for PR #659 regression":
+      ## Data taken from `kdtree` test case, in which the node `split`
+      ## based on `data.percentile(50)` (i.e. `data.median`) suddenly
+      ## changed after PR:
+      ## https://github.com/mratsim/Arraymancer/pull/659/
+      let t = @[@[0.195513 ,       0.225253],
+                @[0.181441 ,       0.102758],
+                @[0.26576  ,       0.218074],
+                @[0.180852 ,     0.00262669],
+                @[0.219789 ,       0.191867],
+                @[0.160581 ,          0.131],
+                @[0.269926 ,       0.237261],
+                @[0.223423 ,       0.232116],
+                @[0.191391 ,       0.183001],
+                @[0.19654  ,      0.0809091],
+                @[0.191497 ,      0.0929182],
+                @[0.22709  ,       0.125705],
+                @[0.263181 ,       0.124787],
+                @[0.204926 ,     0.00688886],
+                @[0.151998 ,      0.0531739],
+                @[0.260266 ,      0.0583248],
+                @[0.214864 ,       0.110506],
+                @[0.247688 ,      0.0732228],
+                @[0.246916 ,       0.204899],
+                @[0.215206 ,       0.202225],
+                @[0.242059 ,       0.102491],
+                @[0.159926 ,       0.115765],
+                @[0.249105 ,       0.200658],
+                @[0.195783 ,       0.123984],
+                @[0.17145  ,      0.0506388],
+                @[0.258146 ,      0.0144846],
+                @[0.215311 ,       0.222503],
+                @[0.266231 ,       0.149363],
+                @[0.178909 ,       0.142174],
+                @[0.263406 ,      0.0867369],
+                @[0.264824 ,       0.221786]
+      ].toTensor
+
+      const exp = 0.124787
+      let arg = t[_, 1]
+      check arg.offset == 1 # offset of 1 due to slicing
+      # `reshape` copies here, because `arg` is a non contiguous tensor. Thus
+      # offset must be reset to 0
+      check arg.reshape([1, arg.size]).offset == 0
+      check t[_, 1].percentile(50) == exp
+
 main()
 GC_fullCollect()